c629a2e6f6646b39486cc40bad7ea4e3a2e35bd7
Matty Southall <matty@southall.me>
2014-01-03 01:12:59 +0000

    Fixed distance calculator

--- src/main/java/com/cometsrv/game/utilities/DistanceCalculator.java
+++ src/main/java/com/cometsrv/game/utilities/DistanceCalculator.java
@@ -7,13 +7,13 @@ public class DistanceCalculator {
 
     public static boolean tilesTouching(int pos1X, int pos1Y, int pos2X, int pos2Y) {
         if (!(Math.abs(pos1X - pos2X) > 1 || Math.abs(pos1Y - pos2Y) > 1)) {
-            return true;
+            return false;
         }
 
         if (pos1X == pos2X && pos1Y == pos2Y) {
-            return true;
+            return false;
         }
 
-        return false;
+        return true;
     }
 }
