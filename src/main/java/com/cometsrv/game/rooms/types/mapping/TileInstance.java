3dca885ac280179a7dfedb4476fed4749eb0b1d0
Leon Hartley <lhartley97@gmail.com>
2014-01-28 19:13:35 +0000

    Start of recode

---
+++ src/main/java/com/cometsrv/game/rooms/types/mapping/TileInstance.java
@@ -0,0 +1,19 @@
+package com.cometsrv.game.rooms.types.mapping;
+
+import com.cometsrv.game.rooms.avatars.misc.Position3D;
+
+public class TileInstance {
+    private RoomMapping mappingInstance;
+    private Position3D position;
+
+    private 
+
+    public TileInstance(RoomMapping mappingInstance, Position3D position) {
+        this.mappingInstance = mappingInstance;
+        this.position = position;
+    }
+
+    public void reload() {
+
+    }
+}
