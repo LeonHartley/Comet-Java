65ba21eb48734c64e5ee246bb8f0266807841240
Leon Hartley <lhartley97@gmail.com>
2014-02-24 15:04:50 +0000

    Attempted fix to broadcasting composers... Still not solved, will try again soon.

--- src/main/java/com/cometproject/server/network/codec/MessageEncoder.java
+++ src/main/java/com/cometproject/server/network/codec/MessageEncoder.java
@@ -2,25 +2,35 @@ package com.cometproject.server.network.codec;
 
 import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
 import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.util.ReferenceCountUtil;
 import org.apache.log4j.Logger;
 
 import java.util.List;
 
-public class MessageEncoder extends MessageToMessageEncoder<Composer> {
+public class MessageEncoder extends MessageToByteEncoder<Composer> {
     private static Logger log = Logger.getLogger(MessageEncoder.class.getName());
 
-    @Override
+/*    @Override
     protected void encode(ChannelHandlerContext ctx, Composer msg, List<Object> out) throws Exception {
-        try {
-            out.add(msg.get());
+       try {
+            ByteBuf bytes = msg.get();
+
+            out.add(bytes);
+            bytes.retain();
 
             ctx.flush();
-
             log.debug("Composed message: " + Composers.valueOfId(msg.getId()));
         } catch(Exception e) {
-            log.error("Failed to encode message : ", e);
+            log.error("Failed to encode message: ", e);
         }
+    }
+*/
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Composer composer, ByteBuf byteBuf) throws Exception {
+        byteBuf.writeBytes(composer.get());
     }
 }
