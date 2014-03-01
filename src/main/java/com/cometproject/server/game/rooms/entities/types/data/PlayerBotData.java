7d5123ba4d3711e8733c50f350f7617654767f77
Leon Hartley <lhartley97@gmail.com>
2014-03-01 21:49:42 +0000

    Guess what?! More work on bots!

--- src/main/java/com/cometproject/server/game/rooms/entities/types/data/PlayerBotData.java
+++ src/main/java/com/cometproject/server/game/rooms/entities/types/data/PlayerBotData.java
@@ -3,7 +3,13 @@ package com.cometproject.server.game.rooms.entities.types.data;
 import com.cometproject.server.game.bots.BotData;
 
 public class PlayerBotData extends BotData {
-    public PlayerBotData(String username, String motto, String figure, String gender) {
+    private int botId;
+
+    public PlayerBotData(int botId, String username, String motto, String figure, String gender) {
         super(username, motto, figure, gender);
+    }
+
+    public int getBotId() {
+        return this.botId;
     }
 }
