6abd2821aeb452071d6f8881057c03a67d951fe4
Leon Hartley <lhartley97@gmail.com>
2014-03-05 21:53:34 +0000

    Cleaned up Julien’s shit.

--- src/main/java/com/cometproject/server/game/commands/staff/DisconnectCommand.java
+++ src/main/java/com/cometproject/server/game/commands/staff/DisconnectCommand.java
@@ -6,24 +6,30 @@ import com.cometproject.server.game.commands.ChatCommand;
 import com.cometproject.server.network.messages.outgoing.misc.AdvancedAlertMessageComposer;
 import com.cometproject.server.network.sessions.Session;
 
-/**
- * Created by Julien on 03/03/2014.
- */
 public class DisconnectCommand extends ChatCommand {
     @Override
     public void execute(Session client, String[] params) {
         String username = params[1];
-        Session UserToDisconnect = Comet.getServer().getNetwork().getSessions().getByPlayerUsername(username);
-        if (UserToDisconnect == client) {
+
+        Session userToDisconnect = Comet.getServer().getNetwork().getSessions().getByPlayerUsername(username);
+
+        if(userToDisconnect == null) {
+            return;
+        }
+
+        if (userToDisconnect == client) {
+            // TODO: put in locale
             client.send(AdvancedAlertMessageComposer.compose("Command error", "You cannot disconnect yourself."));
             return;
         }
-        if (UserToDisconnect.getPlayer().getPermissions().hasPermission("undisconnectable")) { // Perk to add
+
+        if (userToDisconnect.getPlayer().getPermissions().hasPermission("undisconnectable")) { // Perk to add
+            // TODO: put in locale
             client.send(AdvancedAlertMessageComposer.compose("Command error", "You cannot disconnect this player."));
             return;
         }
-        UserToDisconnect.disconnect();
-        return;
+
+        userToDisconnect.disconnect();
     }
 
     @Override
