d6da1b744b315949b9ad50c72fa8850f6a7bb33d
Leon Hartley <lhartley97@gmail.com>
2014-03-09 20:39:17 +0000

    Revert "Encryption still isn't fun. :P"
    This reverts commit 96815cf8fddef9fad14d6ac1bffc0b08203f210d.

--- src/main/java/com/cometproject/server/network/codec/MessageDecoder.java
+++ src/main/java/com/cometproject/server/network/codec/MessageDecoder.java
@@ -1,40 +1,19 @@
 package com.cometproject.server.network.codec;
 
-import com.cometproject.server.boot.Comet;
-import com.cometproject.server.network.NetworkEngine;
 import com.cometproject.server.network.messages.headers.Events;
 import com.cometproject.server.network.messages.types.Event;
-import com.cometproject.server.network.sessions.Session;
-import com.cometproject.server.network.sessions.SessionManager;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
-import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class MessageDecoder extends ByteToMessageDecoder {
-    private Logger log = Logger.getLogger(MessageDecoder.class.getName());
-
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
         try {
             if (msg.readableBytes() < 6) {
                 return;
-            }
-
-            log.debug("Data received! [length: " + msg.readableBytes() + "]");
-
-            Session client = ctx.channel().attr(NetworkEngine.SESSION_ATTRIBUTE_KEY).get();
-            boolean needsDecrypt = true;
-
-            if(client == null || client.getRC4() == null) {
-                needsDecrypt = false;
-            }
-
-            if(needsDecrypt) {
-                msg = Unpooled.copiedBuffer(client.getRC4().Parse(msg.array()));
             }
 
             msg.markReaderIndex();
@@ -47,11 +26,46 @@ public class MessageDecoder extends ByteToMessageDecoder {
 
             short messageId = msg.readShort();
 
+            System.out.println(Events.valueOfId(messageId));
+            System.out.println(Events.valueOfId(messageId));
+            System.out.println(Events.valueOfId(messageId));
+            System.out.println(Events.valueOfId(messageId));
+            System.out.println(Events.valueOfId(messageId));
+            System.out.println(length);
+
             length -= 2;
 
             out.add(new Event(messageId, msg.readBytes(length)));
         } catch (Exception e) {
             e.printStackTrace(); // for debugging!
         }
+
+        /*try {
+            if(msg.readableBytes() < 6) {
+                return;
+            }
+
+            byte[] length = msg.readBytes(4).array();
+
+            if(length[0] == 60) {
+                msg.discardReadBytes();
+
+                ctx.writeAndFlush(
+                        "<?xml version=\"1.0\"?>\r\n"
+                                + "<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\r\n"
+                                + "<cross-domain-policy>\r\n"
+                                + "<allow-access-from domain=\"*\" to-ports=\"*\" />\r\n"
+                                + "</cross-domain-policy>\0"
+                ).addListener(ChannelFutureListener.CLOSE);
+            } else {
+                int messageLength = ByteBuffer.wrap(length).asIntBuffer().get();
+                ByteBuf msgBuffer = msg.readBytes(messageLength);
+
+                short header = msgBuffer.readShort();
+                out.add(new Event(header, msgBuffer));
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
