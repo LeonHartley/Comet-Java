db412681d79afc7dfcece23180b4a8bc957d1c49
Leon Hartley <lhartley97@gmail.com>
2014-03-19 00:36:35 +0000

    room ads + more

--- src/main/java/com/cometproject/server/network/messages/headers/Events.java
+++ src/main/java/com/cometproject/server/network/messages/headers/Events.java
@@ -97,6 +97,7 @@ public class Events {
     public static short PetInventoryMessageEvent = 686;
     public static short PlacePetMessageEvent = 730;
     public static short DropHandItemMessageEvent = 1220;
+    public static short SaveBrandingMessageEvent = 559;
 
     public static short ManageGroupMessageEvent = 454;
     public static short AddUserToRoom2MessageEvent = 3317;
