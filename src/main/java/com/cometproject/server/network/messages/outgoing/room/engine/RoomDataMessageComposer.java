cfd064d74efa989ca2022215268f642bc49342b5
Leon Hartley <lhartley97@gmail.com>
2014-03-27 18:36:58 +0000

    Fixed some bugs with follow (it works flawlessly now!)

--- src/main/java/com/cometproject/server/network/messages/outgoing/room/engine/RoomDataMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/room/engine/RoomDataMessageComposer.java
@@ -7,27 +7,10 @@ import com.cometproject.server.network.messages.types.Composer;
 
 public class RoomDataMessageComposer {
     public static Composer compose(Room room) {
-        return compose(room, false);
-    }
-
-    public static Composer compose(Room room, boolean isFollow) {
         Composer msg = new Composer(Composers.RoomDataMessageComposer);
-        if(isFollow) {
-            msg.writeBoolean(false);
-        }
+
 
         RoomWriter.writeData(room, msg);
-
-        if(isFollow) {
-            msg.writeBoolean(true);
-            msg.writeBoolean(false);
-            msg.writeBoolean(true);
-            msg.writeBoolean(true);
-            msg.writeInt(0);
-            msg.writeInt(0);
-            msg.writeInt(0);
-            msg.writeBoolean(true);
-        }
 
         return msg;
     }
