d116dc7fd99d16a88d1e5cc314ca96a18c2793a5
Leon Hartley <lhartley97@gmail.com>
2014-03-27 21:40:56 +0000

    Badges

--- src/main/java/com/cometproject/server/network/messages/outgoing/user/profile/UserBadgesMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/user/profile/UserBadgesMessageComposer.java
@@ -6,7 +6,25 @@ import com.cometproject.server.network.messages.types.Composer;
 import java.util.Map;
 
 public class UserBadgesMessageComposer {
-    public static Composer compose(Map<String, Integer> badges) {
-        Composer msg = new Composer(Composers.UpdateBadgesMessageComposer);
+    public static Composer compose(int userId, Map<String, Integer> badges, boolean inRoom) {
+        Composer msg = new Composer(Composers.UserBadgesMessageComposer);
+
+        msg.writeInt(userId);
+        msg.writeInt(badges.size());
+
+        for(Map.Entry<String, Integer> badge : badges.entrySet()) {
+            if(badge.getValue() > 0) {
+                msg.writeInt(badge.getValue());
+                msg.writeString(badge.getKey());
+            }
+        }
+
+        if(inRoom) {
+            msg.writeInt(0);
+            msg.writeInt(0);
+            msg.writeInt(0);
+        }
+
+        return msg;
     }
 }
