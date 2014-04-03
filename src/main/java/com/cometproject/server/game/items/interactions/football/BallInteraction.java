9e3b88c2726f7467bc811f268615ddc6c14e2da7
Leon Hartley <lhartley97@gmail.com>
2014-04-03 21:14:50 +0100

    Returned the KICK_POWER to original state

--- src/main/java/com/cometproject/server/game/items/interactions/football/BallInteraction.java
+++ src/main/java/com/cometproject/server/game/items/interactions/football/BallInteraction.java
@@ -140,7 +140,7 @@ public class BallInteraction extends Interactor {
         return new Position3D(x, y);
     }
 
-    public static final int KICK_POWER = 100;
+    public static final int KICK_POWER = 6;
 
     @Override
     public boolean onInteract(int request, RoomItem item, PlayerEntity avatar, boolean isWiredTriggered) {
