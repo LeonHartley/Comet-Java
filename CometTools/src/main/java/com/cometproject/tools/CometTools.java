7e772c7c6b2309826a01ea954cea8f98d24e84a0
Leon Hartley <lhartley97@gmail.com>
2014-04-08 04:14:33 +0100

    Work on CometTools (Added support for dumping packet structures!)

--- CometTools/src/main/java/com/cometproject/tools/CometTools.java
+++ CometTools/src/main/java/com/cometproject/tools/CometTools.java
@@ -2,15 +2,20 @@ package com.cometproject.tools;
 
 import com.cometproject.tools.packets.PacketManager;
 import com.cometproject.tools.ui.CometWindow;
+import com.google.common.base.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
 
 public class CometTools {
     private PacketManager packetManager;
     private CometWindow cometWindow;
 
     public CometTools() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         this.packetManager = new PacketManager();
+        //this.cometWindow = new CometWindow();
 
-        this.cometWindow = new CometWindow();
+        System.out.println("CometTools was active for: " + (((double) stopwatch.elapsed(TimeUnit.MILLISECONDS)) / 1000) + " seconds");
     }
 
     private static CometTools instance;
