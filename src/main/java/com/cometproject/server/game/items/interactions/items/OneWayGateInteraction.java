2d25d400256f48e7cb411c6827dc80d85dca3ab0
Leon Hartley <lhartley97@gmail.com>
2014-04-09 22:10:22 +0100

    One way gates

--- src/main/java/com/cometproject/server/game/items/interactions/items/OneWayGateInteraction.java
+++ src/main/java/com/cometproject/server/game/items/interactions/items/OneWayGateInteraction.java
@@ -1,7 +1,11 @@
 package com.cometproject.server.game.items.interactions.items;
 
+import com.cometproject.server.game.items.interactions.InteractionAction;
+import com.cometproject.server.game.items.interactions.InteractionQueueItem;
 import com.cometproject.server.game.items.interactions.Interactor;
+import com.cometproject.server.game.rooms.avatars.misc.Position3D;
 import com.cometproject.server.game.rooms.entities.types.PlayerEntity;
+import com.cometproject.server.game.rooms.items.FloorItem;
 import com.cometproject.server.game.rooms.items.RoomItem;
 import com.cometproject.server.game.rooms.types.Room;
 
@@ -18,6 +22,21 @@ public class OneWayGateInteraction extends Interactor {
 
     @Override
     public boolean onInteract(int request, RoomItem item, PlayerEntity avatar, boolean isWiredTriggered) {
+        FloorItem floorItem = (FloorItem) item;
+        Position3D doorPosition = new Position3D(floorItem.getX(), floorItem.getY());
+
+        if(doorPosition.squareInFront(item.getRotation()).getX() != avatar.getPosition().getX() && doorPosition.squareInFront(item.getRotation()).getY() != avatar.getPosition().getY()) {
+            avatar.moveTo(doorPosition.squareInFront(item.getRotation()).getX(), doorPosition.squareInFront(item.getRotation()).getY());
+            return false;
+        }
+
+        avatar.setOverriden(true);
+        avatar.moveTo(doorPosition.squareBehind(item.getRotation()).getX(), doorPosition.squareBehind(item.getRotation()).getY());
+
+        item.setExtraData("1");
+        item.sendUpdate();
+
+        item.queueInteraction(new InteractionQueueItem(true, item, InteractionAction.ON_TICK, avatar, 0, 10));
         return false;
     }
 
@@ -33,6 +52,10 @@ public class OneWayGateInteraction extends Interactor {
 
     @Override
     public boolean onTick(RoomItem item, PlayerEntity avatar, int updateState) {
+        item.setExtraData("0");
+        item.sendUpdate();
+
+        avatar.setOverriden(false);
         return false;
     }
 
