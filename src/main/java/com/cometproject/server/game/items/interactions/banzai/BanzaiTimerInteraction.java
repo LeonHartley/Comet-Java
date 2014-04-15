9628d098fc146851c1eb34cee18aab07226fef68
Leon Hartley <lhartley97@gmail.com>
2014-04-15 21:56:02 +0100

    BanzaiTimerInteraction

--- src/main/java/com/cometproject/server/game/items/interactions/banzai/BanzaiTimerInteraction.java
+++ src/main/java/com/cometproject/server/game/items/interactions/banzai/BanzaiTimerInteraction.java
@@ -1,4 +1,53 @@
 package com.cometproject.server.game.items.interactions.banzai;
 
-public class BanzaiTimerInteraction {
+import com.cometproject.server.game.items.interactions.Interactor;
+import com.cometproject.server.game.rooms.entities.types.PlayerEntity;
+import com.cometproject.server.game.rooms.items.RoomItem;
+import com.cometproject.server.game.rooms.types.Room;
+
+public class BanzaiTimerInteraction extends Interactor {
+    @Override
+    public boolean onWalk(boolean state, RoomItem item, PlayerEntity avatar) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreWalk(RoomItem item, PlayerEntity avatar) {
+        return false;
+    }
+
+    @Override
+    public boolean onInteract(int request, RoomItem item, PlayerEntity avatar, boolean isWiredTriggered) {
+        switch(request) {
+            case 2:
+                item.setExtraData("30");
+                item.sendUpdate();
+                break;
+
+            case 0:
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onPlace(RoomItem item, PlayerEntity avatar, Room room) {
+        return false;
+    }
+
+    @Override
+    public boolean onPickup(RoomItem item, PlayerEntity avatar, Room room) {
+        return false;
+    }
+
+    @Override
+    public boolean onTick(RoomItem item, PlayerEntity avatar, int updateState) {
+        return false;
+    }
+
+    @Override
+    public boolean requiresRights() {
+        return false;
+    }
 }
