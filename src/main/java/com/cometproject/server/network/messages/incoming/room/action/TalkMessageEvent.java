9791558229224eca75d07d3741bed5ad7b3765e7
Leon Hartley <lhartley97@gmail.com>
2014-04-24 21:21:14 +0100

    Various chat fixes including the “hack” allowing to spoof usernames

--- src/main/java/com/cometproject/server/network/messages/incoming/room/action/TalkMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/room/action/TalkMessageEvent.java
@@ -7,6 +7,11 @@ import com.cometproject.server.network.messages.outgoing.misc.AdvancedAlertMessa
 import com.cometproject.server.network.messages.outgoing.room.avatar.TalkMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
 import com.cometproject.server.network.sessions.Session;
+import com.google.common.primitives.Ints;
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class TalkMessageEvent implements IEvent {
@@ -17,32 +22,16 @@ public class TalkMessageEvent implements IEvent {
         if (!TalkMessageEvent.isValidColour(colour, client))
             colour = 0;
 
-        if (client.getPlayer().getEntity().getRoom().hasRoomMute() && !client.getPlayer().getPermissions().hasPermission("bypass_roommute")) {
-            return;
-        }
-
-        if (client.getPlayer().getData().getRank() < 7) {
-            if (GameEngine.getRooms().getFilter().filter(message)) {
-                client.send(AdvancedAlertMessageComposer.compose(Locale.get("filter.alert.title"), Locale.get("filter.alert.message")));
-                return;
-            }
-        }
-
         if (client.getPlayer().getEntity().onChat(message)) {
             client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(TalkMessageComposer.compose(client.getPlayer().getEntity().getVirtualId(), message, GameEngine.getRooms().getEmotions().getEmotion(message), colour));
         }
     }
 
+    private static int[] allowedColours = new int[] {
+      0, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 9
+    };
+
     public static boolean isValidColour(int colour, Session client) {
-        if (colour >= 24)
-            return false;
-
-        if (colour == 1 || colour == 2)
-            return false;
-
-        if (colour == 23 && !client.getPlayer().getPermissions().hasPermission("mod_tool"))
-            return false;
-
-        return true;
+        return colour < 24 && Ints.contains(allowedColours, colour) && !(colour == 23 && !client.getPlayer().getPermissions().hasPermission("mod_tool"));
     }
 }
