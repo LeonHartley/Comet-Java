0cc21b95f6345bb26b7cac220176a14f50978652
Leon Hartley <lhartley97@gmail.com>
2014-04-26 20:54:40 +0100

    Max users in room setting fix

--- src/main/java/com/cometproject/server/network/messages/incoming/room/settings/SaveRoomDataMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/room/settings/SaveRoomDataMessageEvent.java
@@ -104,7 +104,7 @@ public class SaveRoomDataMessageEvent implements IEvent {
             data.save();
 
             client.send(ConfigureWallAndFloorMessageComposer.compose(hideWall == 1, wallThick, floorThick));
-            room.getEntities().broadcastMessage(GetRoomDataMessageComposer.compose(room));
+            room.getEntities().broadcastMessage(GetRoomDataMessageComposer.compose(room, client.getPlayer().getPermissions().hasPermission("mod_tool")));
         } catch (Exception e) {
             RoomManager.log.error("Error while saving room data", e);
         }
