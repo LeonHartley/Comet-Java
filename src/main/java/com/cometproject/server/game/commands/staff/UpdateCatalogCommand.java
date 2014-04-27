19e34a8732a99f047a58a42e08107965fe0f1017
Matty Southall <Matty@Mattys-iMac.local>
2014-04-27 13:10:24 +0100

    Update catalog command

---
+++ src/main/java/com/cometproject/server/game/commands/staff/UpdateCatalogCommand.java
@@ -0,0 +1,30 @@
+package com.cometproject.server.game.commands.staff;
+
+import com.cometproject.server.boot.Comet;
+import com.cometproject.server.config.Locale;
+import com.cometproject.server.game.GameEngine;
+import com.cometproject.server.game.commands.ChatCommand;
+import com.cometproject.server.network.sessions.Session;
+
+/**
+ * Created by Matty on 27/04/2014.
+ */
+public class UpdateCatalogCommand extends ChatCommand {
+    @Override
+    public void execute(Session client, String[] params) {
+        GameEngine.getCatalog().loadPages();
+        GameEngine.getCatalog().loadClubOffers();
+
+        this.sendChat("Catalog has been reloaded", client);
+    }
+
+    @Override
+    public String getPermission() {
+        return "updatecatalog_command";
+    }
+
+    @Override
+    public String getDescription() {
+        return Locale.get("command.updatecatalog.description");
+    }
+}
