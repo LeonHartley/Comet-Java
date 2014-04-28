ec4a8f4dc7810bae2f02986d63d79700110ca598
Matty Southall <Matty@Mattys-iMac.local>
2014-04-28 23:45:50 +0100

    Sql Changes

--- src/main/java/com/cometproject/server/game/players/data/PlayerLoader.java
+++ src/main/java/com/cometproject/server/game/players/data/PlayerLoader.java
@@ -4,9 +4,9 @@ import com.cometproject.server.boot.Comet;
 import com.cometproject.server.game.players.types.Player;
 import com.cometproject.server.game.players.types.PlayerSettings;
 import com.cometproject.server.game.players.types.PlayerStatistics;
+import com.cometproject.server.storage.queries.player.PlayerDao;
 import org.apache.log4j.Logger;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -14,18 +14,7 @@ public class PlayerLoader {
     private static Logger log = Logger.getLogger(PlayerLoader.class.getName());
 
     public static Player loadPlayerBySSo(String ticket) {
-        try {
-            PreparedStatement statement = Comet.getServer().getStorage().prepare("SELECT `id` FROM players WHERE auth_ticket = ?");
-
-            statement.setString(1, ticket);
-            ResultSet data = statement.executeQuery();
-
-            return PlayerLoader.resultToPlayer(data);
-        } catch (SQLException e) {
-            log.error("Error while loading player data", e);
-        }
-
-        return null;
+        return new Player(PlayerDao.getIdBySSO(ticket));
     }
 
     public static PlayerSettings loadSettings(int id) {
@@ -72,13 +61,6 @@ public class PlayerLoader {
     }
 
     public static PlayerData loadDataById(int id) {
-        try {
-            ResultSet result = Comet.getServer().getStorage().getRow("SELECT * FROM players WHERE id = " + id + " LIMIT 1;");
-            return new PlayerData(result.getInt("id"), result.getString("username"), result.getString("motto"), result.getString("figure"), result.getString("gender"), result.getInt("rank"), result.getInt("credits"), result.getInt("vip_points"), result.getString("reg_date"), result.getInt("last_online"), result.getString("vip").equals("1"), result.getInt("achievement_points"));
-        } catch (SQLException e) {
-            log.error("Error while loading player data", e);
-        }
-
-        return null;
+        return PlayerDao.getDataById(id);
     }
 }
