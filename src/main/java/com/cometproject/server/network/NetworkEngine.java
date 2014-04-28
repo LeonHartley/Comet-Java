ae49a4778bd21d34f1acf307bd275b67c54708bf
Matty Southall <Matty@Mattys-iMac.local>
2014-04-28 18:21:59 +0100

    Changes to threading

--- src/main/java/com/cometproject/server/network/NetworkEngine.java
+++ src/main/java/com/cometproject/server/network/NetworkEngine.java
@@ -7,7 +7,9 @@ import com.cometproject.server.network.messages.MessageHandler;
 import com.cometproject.server.network.sessions.SessionManager;
 import org.apache.log4j.Logger;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.AdaptiveReceiveBufferSizePredictorFactory;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
@@ -35,7 +37,20 @@ public class NetworkEngine {
                         Executors.newCachedThreadPool()));
 
         bootstrap.setOption("backlog", 1000);
-        bootstrap.setPipelineFactory(new NetworkChannelInitializer());
+        bootstrap.setOption("tcpNoDelay", true);
+
+        // better to have an receive buffer predictor
+        bootstrap.setOption("receiveBufferSizePredictorFactory", new AdaptiveReceiveBufferSizePredictorFactory(128, 1024, 4096));
+
+        //if the server is sending 1000 messages per sec, optimum write buffer water marks will
+        //prevent unnecessary throttling, Check NioSocketChannelConfig doc
+        bootstrap.setOption("writeBufferLowWaterMark", 32 * 1024);
+        bootstrap.setOption("writeBufferHighWaterMark", 64 * 1024);
+
+        int poolSize = (Runtime.getRuntime().availableProcessors() * 2);
+        OrderedMemoryAwareThreadPoolExecutor handlerExecutor = new OrderedMemoryAwareThreadPoolExecutor(poolSize, 0, 0);
+
+        bootstrap.setPipelineFactory(new NetworkChannelInitializer(handlerExecutor));
 
 
         this.ip = ip;
--- src/main/java/com/cometproject/server/tasks/CometThreadManagement.java
+++ src/main/java/com/cometproject/server/tasks/CometThreadManagement.java
@@ -1,14 +1,38 @@
 package com.cometproject.server.tasks;
 
+import java.util.UUID;
 import java.util.concurrent.*;
 
 public class CometThreadManagement {
-    private ExecutorService executionService;
-    private ScheduledExecutorService scheduledExecutorService;
+    private final ExecutorService executionService;
+    private final ScheduledExecutorService scheduledExecutorService;
+
+    private final int initialpoolSize = (Runtime.getRuntime().availableProcessors() * 2);
 
     public CometThreadManagement() {
-        this.executionService = Executors.newCachedThreadPool();
-        this.scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        this.executionService = Executors.newCachedThreadPool(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                UUID randomId = UUID.randomUUID();
+
+                Thread workerThread = new Thread(r);
+                workerThread.setName("Comet-Worker-Thread-" + randomId.toString());
+
+                return workerThread;
+            }
+        });
+
+        this.scheduledExecutorService = Executors.newScheduledThreadPool(this.initialpoolSize, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                UUID randomId = UUID.randomUUID();
+
+                Thread scheduledThread = new Thread(r);
+                scheduledThread.setName("Comet-Scheduled-Thread-" + randomId.toString());
+
+                return scheduledThread;
+            }
+        });
     }
 
     public Future executeOnce(CometTask task) {
