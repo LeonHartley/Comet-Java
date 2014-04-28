1373a5fe3cd06bdf8fd5c354ac193cea620d9db9
Matty Southall <Matty@Mattys-iMac.local>
2014-04-28 18:21:59 +0100

    Changes to threading

--- src/main/java/com/cometproject/server/network/NetworkChannelInitializer.java
+++ src/main/java/com/cometproject/server/network/NetworkChannelInitializer.java
@@ -8,6 +8,8 @@ import com.cometproject.server.network.codec.XMLPolicyDecoder;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 import org.jboss.netty.handler.timeout.IdleStateHandler;
 import org.jboss.netty.util.HashedWheelTimer;
 import org.jboss.netty.util.Timer;
@@ -16,9 +18,13 @@ public class NetworkChannelInitializer implements ChannelPipelineFactory {
     private final Timer idleTimer;
     private final ClientIdleHandler idleHandler;
 
-    public NetworkChannelInitializer() {
+    private final OrderedMemoryAwareThreadPoolExecutor executor;
+
+    public NetworkChannelInitializer(OrderedMemoryAwareThreadPoolExecutor executionExecutor) {
         this.idleTimer = new HashedWheelTimer();
         this.idleHandler = new ClientIdleHandler();
+
+        this.executor = executionExecutor;
     }
 
     @Override
@@ -31,6 +37,7 @@ public class NetworkChannelInitializer implements ChannelPipelineFactory {
         pipeline.addLast("decoder", new MessageDecoder());
         pipeline.addLast("idleStateHandler", new IdleStateHandler(this.idleTimer, 60, 30, 0));
         pipeline.addLast("clientIdleHandler", this.idleHandler);
+        pipeline.addLast("executionHandler", new ExecutionHandler(this.executor));
         pipeline.addLast("handler", new ClientHandler());
 
         return pipeline;
