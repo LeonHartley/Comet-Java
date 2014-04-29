dfeec4ac33362eb71aedd5a97f895395d01248d5
Leon Hartley <lhartley97@gmail.com>
2014-04-29 23:00:22 +0100

    Change height map to use base 36 instead of a character array

--- src/main/java/com/cometproject/server/network/messages/outgoing/room/engine/RelativeHeightmapMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/room/engine/RelativeHeightmapMessageComposer.java
@@ -6,17 +6,7 @@ import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
 
 public class RelativeHeightmapMessageComposer {
-    private static char[] characters;
-
-    private static void init() {
-        characters = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
-    }
-
     public static Composer compose(RoomModel model) {
-        if (characters == null) {
-            init();
-        }
-
         Composer msg = new Composer(Composers.RelativeHeightmapMessageComposer);
 
         msg.writeBoolean(true); // ??
@@ -30,7 +20,7 @@ public class RelativeHeightmapMessageComposer {
                 } else if(model.getSquareState()[x][y] == RoomTileState.INVALID) {
                     builder.append("x");
                 } else {
-                    builder.append(characters[(int) Math.floor(model.getSquareHeight()[x][y] + 0.5d)]);
+                    builder.append(Integer.toString((int) Math.floor(model.getSquareHeight()[x][y] + 0.5d), 36));
                 }
             }
 
