1f9ab3b068f26f8bd154ccbac4f746ff4f83c329
Matty Southall <Matty@Mattys-iMac.local>
2014-05-02 16:23:39 +0100

    Add Trade Sql

---
+++ src/main/java/com/cometproject/server/storage/queries/items/TradeDao.java
@@ -0,0 +1,33 @@
+package com.cometproject.server.storage.queries.items;
+
+import com.cometproject.server.storage.SqlHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by Matty on 02/05/2014.
+ */
+public class TradeDao {
+
+    public static void updateTradeItems(int userId, int itemId) {
+        Connection sqlConnection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            sqlConnection = SqlHelper.getConnection();
+
+            preparedStatement = SqlHelper.prepare("UPDATE items SET user_id = ? AND room_id = 0 WHERE id = ?", sqlConnection);
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(2, itemId);
+
+            SqlHelper.executeStatementSilently(preparedStatement, false);
+        } catch (SQLException e) {
+            SqlHelper.handleSqlException(e);
+        } finally {
+            SqlHelper.closeSilently(preparedStatement);
+            SqlHelper.closeSilently(sqlConnection);
+        }
+    }
+}
