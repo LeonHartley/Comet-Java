1be9007c6a46676ef80f645c90c90f1e3eb3cc7e
Leon Hartley <lhartley97@gmail.com>
2014-05-05 21:18:37 +0100

    PrivateChat flooding

--- src/main/java/com/cometproject/server/network/messages/incoming/messenger/PrivateChatMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/messenger/PrivateChatMessageEvent.java
@@ -3,6 +3,7 @@ package com.cometproject.server.network.messages.incoming.messenger;
 import com.cometproject.server.game.players.components.types.MessengerFriend;
 import com.cometproject.server.network.messages.incoming.IEvent;
 import com.cometproject.server.network.messages.outgoing.messenger.InstantChatMessageComposer;
+import com.cometproject.server.network.messages.outgoing.room.permissions.FloodFilterMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
 import com.cometproject.server.network.sessions.Session;
 
@@ -28,6 +29,26 @@ public class PrivateChatMessageEvent implements IEvent {
         if (friendClient == null) {
             return;
         }
+
+        long time = System.currentTimeMillis();
+
+        if (time - client.getPlayer().lastMessage < 750) { // TODO: add flood bypass for staff with permission or something
+            client.getPlayer().floodFlag++;
+
+            if (client.getPlayer().floodFlag >= 4) {
+                client.getPlayer().floodTime = 30;
+                client.getPlayer().floodFlag = 0;
+
+                client.send(FloodFilterMessageComposer.compose(client.getPlayer().floodTime));
+            }
+        }
+
+        if (client.getPlayer().floodTime >= 1) {
+            return;
+        }
+
+        client.getPlayer().lastMessage = time;
+
 
         friendClient.send(InstantChatMessageComposer.compose(message, client.getPlayer().getId()));
     }
