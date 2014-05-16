f5456536ccda53cfbcc5dc8c765a7b660e96fb8e
Leon Hartley <lhartley97@gmail.com>
2014-05-16 21:13:02 +0100

    Patched whisper "exploit"

--- src/main/java/com/cometproject/server/network/messages/incoming/room/action/WisperMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/room/action/WisperMessageEvent.java
@@ -1,7 +1,9 @@
 package com.cometproject.server.network.messages.incoming.room.action;
 
-import com.cometproject.server.boot.Comet;
 import com.cometproject.server.game.CometManager;
+import com.cometproject.server.game.rooms.entities.GenericEntity;
+import com.cometproject.server.game.rooms.entities.RoomEntityType;
+import com.cometproject.server.game.rooms.entities.types.PlayerEntity;
 import com.cometproject.server.network.messages.incoming.IEvent;
 import com.cometproject.server.network.messages.outgoing.room.avatar.WisperMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
@@ -13,9 +15,10 @@ public class WisperMessageEvent implements IEvent {
 
         String user = text.split(" ")[0];
         String message = text.replace(user + " ", "");
-        Session userTo = Comet.getServer().getNetwork().getSessions().getByPlayerUsername(user);
 
-        if (userTo == null || userTo == client)
+        GenericEntity userTo = client.getPlayer().getEntity().getRoom().getEntities().getEntityByName(user, RoomEntityType.PLAYER);
+
+        if (userTo == null || user.equals(client.getPlayer().getData().getUsername()))
             return;
 
         String filteredMessage = TalkMessageEvent.filterMessage(message);
@@ -28,6 +31,6 @@ public class WisperMessageEvent implements IEvent {
             return;
 
         client.send(WisperMessageComposer.compose(client.getPlayer().getEntity().getVirtualId(), filteredMessage));
-        userTo.send(WisperMessageComposer.compose(client.getPlayer().getEntity().getVirtualId(), filteredMessage));
+        ((PlayerEntity) userTo).getPlayer().getSession().send(WisperMessageComposer.compose(client.getPlayer().getEntity().getVirtualId(), filteredMessage));
     }
 }
