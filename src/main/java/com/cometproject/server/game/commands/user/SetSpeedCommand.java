fd083ac081f0b7376bc3405eaf7a2dfd24cbc8e1
Leon Hartley <lhartley97@gmail.com>
2014-05-19 22:07:45 +0100

    SetSpeed now requires room rights

--- src/main/java/com/cometproject/server/game/commands/user/SetSpeedCommand.java
+++ src/main/java/com/cometproject/server/game/commands/user/SetSpeedCommand.java
@@ -13,13 +13,17 @@ public class SetSpeedCommand extends ChatCommand {
 
         if (client.getPlayer().getEntity() != null
                 && client.getPlayer().getEntity().getRoom() != null) {
+            if(!client.getPlayer().getEntity().getRoom().getRights().hasRights(client.getPlayer().getId()) && !client.getPlayer().getPermissions().hasPermission("room_full_control")) {
+                return;
+            }
+
             int speed = Integer.parseInt(params[0]);
 
             if (speed < 1) { speed = 1; }
             if (speed > 20) { speed = 20; }
 
             client.getPlayer().getEntity().getRoom().setAttribute("setspeed", speed);
-            this.sendChat("Roller speed set", client);
+            sendChat("Roller speed set", client);
         }
     }
 
