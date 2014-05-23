f126781258f4a78f3a65bea91b6ee23994b294f5
Matty Southall <Matty@Mattys-iMac.local>
2014-05-23 19:23:06 +0100

    Beds are back

---
+++ src/main/java/com/cometproject/server/game/rooms/items/types/floor/BedFloorItem.java
@@ -0,0 +1,26 @@
+package com.cometproject.server.game.rooms.items.types.floor;
+
+import com.cometproject.server.game.rooms.entities.GenericEntity;
+import com.cometproject.server.game.rooms.items.RoomItemFloor;
+
+public class BedFloorItem extends RoomItemFloor {
+    public BedFloorItem(int id, int itemId, int roomId, int owner, int x, int y, double z, int rotation, String data) {
+        super(id, itemId, roomId, owner, x, y, z, rotation, data);
+    }
+
+    @Override
+    public void onEntityStepOn(GenericEntity entity) {
+        double height = this.getDefinition().getHeight();
+
+        entity.setBodyRotation(this.getRotation());
+        entity.setHeadRotation(this.getRotation());
+        entity.addStatus("lay", String.valueOf(height).replace(',', '.'));
+        entity.markNeedsUpdate();
+    }
+
+    @Override
+    public void onEntityStepOff(GenericEntity entity) {
+        entity.removeStatus("lay");
+        entity.markNeedsUpdate();
+    }
+}
