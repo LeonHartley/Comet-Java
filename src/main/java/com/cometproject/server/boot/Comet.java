c7d34b3e04227a00431c7c8fe6954f5ab17dd76f
Leon Hartley <lhartley97@gmail.com>
2014-05-28 13:26:37 +0100

    0.8.5-ALPHA1

--- src/main/java/com/cometproject/server/boot/Comet.java
+++ src/main/java/com/cometproject/server/boot/Comet.java
@@ -152,7 +152,7 @@ public class Comet {
     }
 
     public static String getBuild() {
-        return "0.8.4.ALPHA7";
+        return "0.8.5.ALPHA1";
     }
 
     public static CometServer getServer() {
