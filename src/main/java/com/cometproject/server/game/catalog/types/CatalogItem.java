0e81346c5c9e999a17b29b8b55d141687aad2f93
Matty <Matty@Mattys-iMac.local>
2014-06-05 00:18:28 +0100

    More small fixes and changes

--- src/main/java/com/cometproject/server/game/catalog/types/CatalogItem.java
+++ src/main/java/com/cometproject/server/game/catalog/types/CatalogItem.java
@@ -55,9 +55,9 @@ public class CatalogItem {
             this.items.add(Integer.valueOf(this.itemId));
         }
 
-        items.stream().filter(id -> CometManager.getItems().getDefintionNullable(id) == null).forEach(id -> {
+        /*items.stream().filter(id -> CometManager.getItems().getDefintionNullable(id) == null).forEach(id -> {
             // do something like delete or w/e idk, no need to output cos the getDefinition will output if its null ;P
-        });
+        });*/
     }
 
     public int getId() {
