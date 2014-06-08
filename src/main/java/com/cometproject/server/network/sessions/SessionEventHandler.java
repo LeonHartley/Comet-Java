ab8421f12f9ebc7232f48da9dc28539617d63ce8
Matty <Matty@Mattys-iMac.local>
2014-06-08 14:20:04 +0100

    Only login events can be called once

--- src/main/java/com/cometproject/server/network/sessions/SessionEventHandler.java
+++ src/main/java/com/cometproject/server/network/sessions/SessionEventHandler.java
@@ -1,5 +1,7 @@
 package com.cometproject.server.network.sessions;
 
+import com.cometproject.server.boot.Comet;
+import com.cometproject.server.network.messages.headers.Events;
 import com.cometproject.server.network.messages.types.Event;
 
 import java.util.HashMap;
@@ -7,16 +9,29 @@ import java.util.Map;
 
 public class SessionEventHandler {
     private final Session session;
-    private final Map<Integer, Boolean> enabledEvents;
+    private final Map<Short, Boolean> loginEvents;
 
     public SessionEventHandler(Session session) {
         this.session = session;
-        this.enabledEvents = new HashMap<>();
+
+        this.loginEvents = new HashMap<Short, Boolean>() {{
+            put(Events.GenerateSecretKeyMessageEvent, true);
+            put(Events.InitCryptoMessageEvent, true);
+            put(Events.SSOTicketMessageEvent, true);
+        }};
     }
 
     public void handle(Event msg) {
-        if (!this.enabledEvents.containsKey(msg.getId())) {
-            return;
+        // Checks if the event is a login event, if it is then check if it is enabled, if it is then disable it because the
+        // event can only be called once!
+        if (this.loginEvents.containsKey(msg.getId())) {
+            if (!this.loginEvents.get(msg.getId())) {
+                return; // event not allowed
+            } else {
+                this.loginEvents.replace(msg.getId(), false);
+            }
         }
+
+        Comet.getServer().getNetwork().getMessages().handle(msg, this.session);
     }
 }
