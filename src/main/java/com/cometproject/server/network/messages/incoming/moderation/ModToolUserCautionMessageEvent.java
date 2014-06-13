0e9963cdd1ece2a8239a1d52832238dfa55716d5
Leon Hartley <lhartley97@gmail.com>
2014-06-13 23:10:42 +0000

    Mod tool player alerts

---
+++ src/main/java/com/cometproject/server/network/messages/incoming/moderation/ModToolUserCautionMessageEvent.java
@@ -0,0 +1,26 @@
+package com.cometproject.server.network.messages.incoming.moderation;
+
+import com.cometproject.server.boot.Comet;
+import com.cometproject.server.game.CometManager;
+import com.cometproject.server.network.messages.incoming.IEvent;
+import com.cometproject.server.network.messages.outgoing.misc.AdvancedAlertMessageComposer;
+import com.cometproject.server.network.messages.types.Event;
+import com.cometproject.server.network.sessions.Session;
+
+public class ModToolUserCautionMessageEvent implements IEvent {
+    @Override
+    public void handle(Session client, Event msg) throws Exception {
+        int playerId = msg.readInt();
+        String message = msg.readString();
+
+        if(CometManager.getPlayers().isOnline(playerId)) {
+            Session session = Comet.getServer().getNetwork().getSessions().getByPlayerId(playerId);
+
+            if(session != null) {
+                session.send(AdvancedAlertMessageComposer.compose(message));
+            }
+        }
+
+        // TODO: Caution player...
+    }
+}
