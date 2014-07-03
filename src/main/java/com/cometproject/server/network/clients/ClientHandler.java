b38140ae4927918c8c93c79bc900b9571d0e6212
Matty Southall <Matty@Mattys-iMac.local>
2014-07-03 17:43:06 +0100

    Close (it what they do idk the difference but meh)

--- src/main/java/com/cometproject/server/network/clients/ClientHandler.java
+++ src/main/java/com/cometproject/server/network/clients/ClientHandler.java
@@ -56,7 +56,7 @@ public class ClientHandler extends SimpleChannelInboundHandler<Event> {
         if (evt instanceof IdleStateEvent) {
             IdleStateEvent e = (IdleStateEvent) evt;
             if (e.state() == IdleState.READER_IDLE) {
-                ctx.disconnect();
+                ctx.close();
             } else if (e.state() == IdleState.WRITER_IDLE) {
                 ctx.writeAndFlush(PingMessageComposer.compose());
             }
