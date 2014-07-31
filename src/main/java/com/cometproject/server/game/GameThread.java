de95ac961b3310c8a26b7bf7014a693a8e5ac3f1
Leon Hartley <lhartley97@gmail.com>
2014-07-31 22:07:04 +0100

    Remove old cache code and work on groups some-what

--- src/main/java/com/cometproject/server/game/GameThread.java
+++ src/main/java/com/cometproject/server/game/GameThread.java
@@ -7,62 +7,24 @@ import com.cometproject.server.network.sessions.Session;
 import com.cometproject.server.storage.queries.system.StatisticsDao;
 import com.cometproject.server.tasks.CometTask;
 import com.cometproject.server.tasks.CometThreadManagement;
-import com.cometproject.server.utilities.TimeSpan;
 import org.apache.log4j.Logger;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class GameThread implements CometTask {
     private static Logger log = Logger.getLogger(GameThread.class.getName());
 
-    private CometThreadManagement threadManagement;
-
     private ScheduledFuture gameFuture;
-    private ScheduledFuture dailyCycleFuture;
 
     private boolean active = false;
 
     private int onlineRecord = 0;
 
     public GameThread(CometThreadManagement mgr) {
-        this.threadManagement = mgr;
-
         int interval = Integer.parseInt(Comet.getServer().getConfig().get("comet.game.thread.interval"));
         this.gameFuture = mgr.executePeriodic(this, interval, interval, TimeUnit.MINUTES);
         this.active = true;
-
-        //this.configureDailyCycle();
-    }
-
-    private void configureDailyCycle() {
-        Calendar c = Calendar.getInstance();
-        Date now = new Date();
-
-        c.setTime(now);
-        c.add(Calendar.DAY_OF_MONTH, 1);
-        c.set(Calendar.HOUR_OF_DAY, 0);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MILLISECOND, 0);
-
-        long msTillMidnight = (c.getTimeInMillis() - now.getTime());
-
-        this.dailyCycleFuture = this.threadManagement.executePeriodic(new CometTask() {
-            @Override
-            public void run() {
-                long start = System.currentTimeMillis();
-
-                //Comet.getServer().getStorage().execute("UPDATE player_stats SET daily_respects = 3 WHERE daily_respects < 3");
-
-
-                TimeSpan span = new TimeSpan(start, System.currentTimeMillis());
-
-                log.info("Daily task has cycled. Took: " + span.toMilliseconds() + "ms to execute.");
-            }
-        }, msTillMidnight, 86400000, TimeUnit.MILLISECONDS);
     }
 
     private int cycleCount = 0;
@@ -119,7 +81,6 @@ public class GameThread implements CometTask {
     public void stop() {
         this.active = false;
         this.gameFuture.cancel(false);
-        this.dailyCycleFuture.cancel(false);
     }
 
     public int getOnlineRecord() {
