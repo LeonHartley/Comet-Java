e88a7c962644486e131b1b19a6187c1751646dce
Leon Hartley <lhartley97@gmail.com>
2014-08-10 23:37:08 +0100

    Comet 0.9.1-BETA1

--- src/main/java/com/cometproject/server/game/commands/notifications/NotificationManager.java
+++ src/main/java/com/cometproject/server/game/commands/notifications/NotificationManager.java
@@ -1,9 +1,11 @@
 package com.cometproject.server.game.commands.notifications;
 
+import javolution.util.FastMap;
+
 import java.util.Map;
 
 public class NotificationManager {
-    private Map<String, Notification> notifications;
+    private Map<String, Notification> notifications = new FastMap<>();
 
     public boolean isNotificationExecutor(String text) {
         return this.notifications.containsKey(text);
