9245cfdd8781e66f70f8683b2964590884006c1f
Leon Hartley <lhartley97@gmail.com>
2014-08-11 22:16:16 +0100

    0.9.1-BETA2

--- src/main/java/com/cometproject/server/boot/Comet.java
+++ src/main/java/com/cometproject/server/boot/Comet.java
@@ -108,7 +108,7 @@ public class Comet {
      * @return The current build of Comet
      */
     public static String getBuild() {
-        return "0.9.1-BETA1";
+        return "0.9.1-BETA2";
     }
 
     /**
--- src/main/java/com/cometproject/server/network/messages/outgoing/room/access/DoorbellRequestComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/room/access/DoorbellRequestComposer.java
@@ -3,9 +3,6 @@ package com.cometproject.server.network.messages.outgoing.room.access;
 import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
 
-/**
- * Created by Matty on 07/05/2014.
- */
 public class DoorbellRequestComposer {
     public static Composer compose(String username) {
         Composer msg = new Composer(Composers.DoorBellRequestComposer);
