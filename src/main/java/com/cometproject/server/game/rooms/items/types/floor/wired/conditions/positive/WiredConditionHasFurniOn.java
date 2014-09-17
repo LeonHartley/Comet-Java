7d6cdb7bddffa49fe84dc071cee29a0d78fb72a3
Leon Hartley <lhartley97@gmail.com>
2014-09-17 21:52:00 +0100

    WiredConditionHasFurniOn

---
+++ src/main/java/com/cometproject/server/game/rooms/items/types/floor/wired/conditions/positive/WiredConditionHasFurniOn.java
@@ -0,0 +1,46 @@
+package com.cometproject.server.game.rooms.items.types.floor.wired.conditions.positive;
+
+import com.cometproject.server.game.rooms.entities.GenericEntity;
+import com.cometproject.server.game.rooms.items.RoomItemFloor;
+import com.cometproject.server.game.rooms.items.types.floor.wired.base.WiredConditionItem;
+
+public class WiredConditionHasFurniOn extends WiredConditionItem {
+    /**
+     * The default constructor
+     *
+     * @param id       The ID of the item
+     * @param itemId   The ID of the item definition
+     * @param roomId   The ID of the room
+     * @param owner    The ID of the owner
+     * @param x        The position of the item on the X axis
+     * @param y        The position of the item on the Y axis
+     * @param z        The position of the item on the z axis
+     * @param rotation The orientation of the item
+     * @param data     The JSON object associated with this item
+     */
+    public WiredConditionHasFurniOn(int id, int itemId, int roomId, int owner, int x, int y, double z, int rotation, String data) {
+        super(id, itemId, roomId, owner, x, y, z, rotation, data);
+    }
+
+    @Override
+    public int getInterface() {
+        return 4;
+    }
+
+    @Override
+    public boolean evaluate(GenericEntity entity, Object data) {
+        boolean hasFurniOnTop = false;
+
+        for(int itemId : this.getWiredData().getSelectedIds()) {
+            RoomItemFloor floorItem = this.getRoom().getItems().getFloorItem(itemId);
+
+            if(floorItem != null) {
+                for(RoomItemFloor itemOnSq : floorItem.getItemsOnStack()) {
+                    if(itemOnSq.getHeight() > this.getHeight()) hasFurniOnTop = true;
+                }
+            }
+        }
+
+        return this.isNegative ? !hasFurniOnTop : hasFurniOnTop;
+    }
+}
