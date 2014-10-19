13ebfef12a905000b4fe2aa8ea3f131e918962c3
Leon Hartley <lhartley97@gmail.com>
2014-10-19 16:05:41 +0100

    Apply all changes to master branch

--- src/main/java/com/cometproject/server/logging/containers/LogEntryContainer.java
+++ src/main/java/com/cometproject/server/logging/containers/LogEntryContainer.java
@@ -4,6 +4,7 @@ import com.cometproject.server.logging.AbstractLogEntry;
 import com.cometproject.server.logging.database.queries.LogQueries;
 
 public class LogEntryContainer {
+    // TODO: Queue these and insert into database periodically
     public void put(AbstractLogEntry logEntry) {
         LogQueries.putEntry(logEntry);
     }
