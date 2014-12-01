27af469fdae2f873f61a3d84e0dbc6ccb19ddcba
Leon Hartley <lhartley97@gmail.com>
2014-12-01 19:55:59 +0000

    Disable the effect from snowboard slope (in code)

--- src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/snowboarding/SnowboardSlopeFloorItem.java
+++ src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/snowboarding/SnowboardSlopeFloorItem.java
@@ -14,20 +14,21 @@ public class SnowboardSlopeFloorItem extends AdjustableHeightFloorItem {
     public SnowboardSlopeFloorItem(int id, int itemId, Room room, int owner, int x, int y, double z, int rotation, String data) {
         super(id, itemId, room, owner, x, y, z, rotation, data);
     }
-
-    @Override
-    public void onEntityStepOn(GenericEntity entity) {
-        if(entity.getCurrentEffect() == null || entity.getCurrentEffect().getEffectId() != getEffectId(entity.getGender())) {
-            entity.applyEffect(new PlayerEffect(getEffectId(entity.getGender()), 0));
-        }
-    }
-
-    @Override
-    public void onEntityStepOff(GenericEntity entity) {
-        if(entity.getCurrentEffect() != null && entity.getCurrentEffect().getEffectId() == getEffectId(entity.getGender())) {
-            entity.applyEffect(null);
-        }
-    }
+//
+//    @Override
+//    public void onEntityStepOn(GenericEntity entity) {
+//        System.out.println("Walked on");
+//        if(entity.getCurrentEffect() == null || entity.getCurrentEffect().getEffectId() != getEffectId(entity.getGender())) {
+//            entity.applyEffect(new PlayerEffect(getEffectId(entity.getGender()), 0));
+//        }
+//    }
+//
+//    @Override
+//    public void onEntityStepOff(GenericEntity entity) {
+//        if(entity.getCurrentEffect() != null && entity.getCurrentEffect().getEffectId() == getEffectId(entity.getGender())) {
+//            entity.applyEffect(null);
+//        }
+//    }
 
     private static int getEffectId(String gender) {
         if(gender.toLowerCase().equals("m")) {
