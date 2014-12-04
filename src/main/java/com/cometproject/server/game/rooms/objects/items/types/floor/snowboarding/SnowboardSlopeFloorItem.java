95558e8e891f1a8b072e495ca387ae971d7c0b8d
Leon Hartley <lhartley97@gmail.com>
2014-12-04 20:59:48 +0000

    Removed unused imports

--- src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/snowboarding/SnowboardSlopeFloorItem.java
+++ src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/snowboarding/SnowboardSlopeFloorItem.java
@@ -1,11 +1,7 @@
 package com.cometproject.server.game.rooms.objects.items.types.floor.snowboarding;
 
-import com.cometproject.server.game.rooms.objects.entities.GenericEntity;
-import com.cometproject.server.game.rooms.objects.entities.RoomEntityStatus;
-import com.cometproject.server.game.rooms.objects.entities.effects.PlayerEffect;
 import com.cometproject.server.game.rooms.objects.items.types.floor.AdjustableHeightFloorItem;
 import com.cometproject.server.game.rooms.types.Room;
-import org.apache.commons.lang.StringUtils;
 
 public class SnowboardSlopeFloorItem extends AdjustableHeightFloorItem {
     public SnowboardSlopeFloorItem(int id, int itemId, Room room, int owner, int x, int y, double z, int rotation, String data) {
