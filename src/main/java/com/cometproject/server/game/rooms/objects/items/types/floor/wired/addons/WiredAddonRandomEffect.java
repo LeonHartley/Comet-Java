4a1c6cb620196188164ab41eedcb843950ca7e39
Leon Hartley <lhartley97@gmail.com>
2014-12-08 17:16:18 +0000

    wf_xtra_random, wf_xtra_unseen

---
+++ src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/wired/addons/WiredAddonRandomEffect.java
@@ -0,0 +1,10 @@
+package com.cometproject.server.game.rooms.objects.items.types.floor.wired.addons;
+
+import com.cometproject.server.game.rooms.objects.items.RoomItemFloor;
+import com.cometproject.server.game.rooms.types.Room;
+
+public class WiredAddonRandomEffect extends RoomItemFloor {
+    public WiredAddonRandomEffect(int id, int itemId, Room room, int owner, int x, int y, double z, int rotation, String data) {
+        super(id, itemId, room, owner, x, y, z, rotation, data);
+    }
+}
