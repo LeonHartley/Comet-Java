6157939083066209caf4e63337893d9c15ae4edf
Leon Hartley <lhartley97@gmail.com>
2014-12-29 12:33:13 +0000

    Move floor item compose to InventoryItem class

--- src/main/java/com/cometproject/server/game/players/components/types/InventoryItem.java
+++ src/main/java/com/cometproject/server/game/players/components/types/InventoryItem.java
@@ -1,12 +1,15 @@
 package com.cometproject.server.game.players.components.types;
 
 import com.cometproject.server.game.catalog.types.gifts.GiftData;
+import com.cometproject.server.game.groups.GroupManager;
+import com.cometproject.server.game.groups.types.GroupData;
 import com.cometproject.server.game.items.ItemManager;
 import com.cometproject.server.game.items.rares.LimitedEditionItem;
 import com.cometproject.server.game.items.types.ItemDefinition;
 import com.cometproject.server.network.messages.types.Composer;
 import com.cometproject.server.storage.queries.items.LimitedEditionDao;
 import com.cometproject.server.utilities.JsonFactory;
+import org.apache.commons.lang.StringUtils;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -51,6 +54,86 @@ public class InventoryItem {
         this.baseId = baseId;
         this.extraData = extraData;
         this.giftData = giftData;
+    }
+
+    public void compose(Composer msg) {
+        final boolean isGift = this.getGiftData() != null;
+        final boolean isGroupItem = this.getDefinition().getInteraction().equals("group_item") || this.getDefinition().getInteraction().equals("group_gate");
+        final boolean isLimited = this.getLimitedEditionItem() != null;
+
+        msg.writeInt(this.getId());
+        msg.writeString(this.getDefinition().getType().toUpperCase());
+        msg.writeInt(this.getId());
+        msg.writeInt(isGift ? this.getGiftData().getSpriteId() : this.getDefinition().getSpriteId());
+
+        if (!isGroupItem)
+            msg.writeInt(1);
+
+        if (isGroupItem) {
+            // Append the group data...
+            int groupId = 0;
+
+            msg.writeInt(17);
+
+            if (StringUtils.isNumeric(this.getExtraData())) {
+                groupId = Integer.parseInt(this.getExtraData());
+            }
+
+            GroupData groupData = groupId == 0 ? null : GroupManager.getInstance().getData(groupId);
+
+            if (groupData == null) {
+                msg.writeInt(0);
+            } else {
+                msg.writeInt(2);
+                msg.writeInt(5);
+                msg.writeString("0"); //state
+                msg.writeString(groupId);
+                msg.writeString(groupData.getBadge());
+
+                String colourA = GroupManager.getInstance().getGroupItems().getSymbolColours().get(groupData.getColourA()) != null ? GroupManager.getInstance().getGroupItems().getSymbolColours().get(groupData.getColourA()).getColour() : "ffffff";
+                String colourB = GroupManager.getInstance().getGroupItems().getBackgroundColours().get(groupData.getColourB()) != null ? GroupManager.getInstance().getGroupItems().getBackgroundColours().get(groupData.getColourB()).getColour() : "ffffff";
+
+                msg.writeString(colourA);
+                msg.writeString(colourB);
+            }
+        } else if (isLimited) {
+            msg.writeString("");
+            msg.writeBoolean(true);
+            msg.writeBoolean(false);
+        } else if (this.getDefinition().getInteraction().equals("badge_display") && !isGift) {
+            msg.writeInt(2);
+        } else {
+            msg.writeInt(0);
+        }
+
+        if (this.getDefinition().getInteraction().equals("badge_display") && !isGift) {
+            msg.writeInt(4);
+
+            msg.writeString("0");
+            msg.writeString(this.getExtraData());
+            msg.writeString(""); // creator
+            msg.writeString(""); // date
+        } else if (!isGroupItem) {
+            msg.writeString(!isGift ? this.getExtraData() : "");
+        }
+
+        if (isLimited && !isGift) {
+            LimitedEditionItem limitedEditionItem = this.getLimitedEditionItem();
+
+            msg.writeInt(limitedEditionItem.getLimitedRare());
+            msg.writeInt(limitedEditionItem.getLimitedRareTotal());
+        }
+
+        msg.writeBoolean(this.getDefinition().canRecycle());
+        msg.writeBoolean(!isGift && this.getDefinition().canTrade());
+        msg.writeBoolean(!isLimited && !isGift && this.getDefinition().canInventoryStack());
+        msg.writeBoolean(!isGift && this.getDefinition().canMarket());
+
+        msg.writeInt(-1);
+        msg.writeBoolean(true);//??
+        msg.writeInt(-1);
+        msg.writeString("");
+        msg.writeInt(isGift ? this.getGiftData().getWrappingPaper() * 1000 + this.getGiftData().getDecorationType() : 0);
     }
 
     public void serializeTrade(Composer msg) {
