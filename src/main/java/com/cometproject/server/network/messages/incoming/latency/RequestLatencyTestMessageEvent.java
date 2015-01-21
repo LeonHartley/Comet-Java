5739ccd978b15c13c0340fe0a09665a5d74943c3
Leon Hartley <lhartley97@gmail.com>
2015-01-21 22:59:49 +0000

    Ping

--- src/main/java/com/cometproject/server/network/messages/incoming/latency/RequestLatencyTestMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/latency/RequestLatencyTestMessageEvent.java
@@ -1,6 +1,7 @@
 package com.cometproject.server.network.messages.incoming.latency;
 
 import com.cometproject.server.network.messages.incoming.IEvent;
+import com.cometproject.server.network.messages.outgoing.misc.PingMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
 import com.cometproject.server.network.sessions.Session;
 
@@ -8,6 +9,6 @@ import com.cometproject.server.network.sessions.Session;
 public class RequestLatencyTestMessageEvent implements IEvent {
     @Override
     public void handle(Session client, Event msg) throws Exception {
-
+        client.send(PingMessageComposer.compose());
     }
 }
--- src/main/java/com/cometproject/server/network/messages/outgoing/misc/PingMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/misc/PingMessageComposer.java
@@ -3,13 +3,8 @@ package com.cometproject.server.network.messages.outgoing.misc;
 import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
 
-
-/**
- * Created by Matty on 26/04/2014.
- */
 public class PingMessageComposer {
     public static Composer compose() {
-        Composer msg = new Composer(Composers.PingMessageComposer);
-        return msg;
+        return new Composer(Composers.PingMessageComposer);
     }
 }
