d801e31306d4d445244510766e5edb540e85e24e
Leon Hartley <lhartley97@gmail.com>
2015-03-06 18:10:08 +0000

    More composers rewritten

--- src/main/java/com/cometproject/server/network/messages/outgoing/messenger/FriendRequestsMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/messenger/FriendRequestsMessageComposer.java
@@ -1,29 +1,39 @@
 package com.cometproject.server.network.messages.outgoing.messenger;
 
 import com.cometproject.server.game.players.data.PlayerAvatar;
+import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
 
 import java.util.List;
 
 
-public class FriendRequestsMessageComposer {
-    public static Composer compose(List<PlayerAvatar> requests) {
-        Composer msg = new Composer(Composers.FriendRequestsMessageComposer);
+public class FriendRequestsMessageComposer extends MessageComposer {
+    private final List<PlayerAvatar> requests;
 
-        try {
-            msg.writeInt(requests.size());
-            msg.writeInt(requests.size());
+    public FriendRequestsMessageComposer(final List<PlayerAvatar> requests) {
+        this.requests = requests;
+    }
 
-            for (PlayerAvatar avatar : requests) {
-                msg.writeInt(avatar.getId());
-                msg.writeString(avatar.getUsername());
-                msg.writeString(avatar.getFigure());
-            }
-        } finally {
-            requests.clear();
+    @Override
+    public short getId() {
+        return Composers.FriendRequestsMessageComposer;
+    }
+
+    @Override
+    public void compose(Composer msg) {
+        msg.writeInt(this.requests.size());
+        msg.writeInt(this.requests.size());
+
+        for (PlayerAvatar avatar : this.requests) {
+            msg.writeInt(avatar.getId());
+            msg.writeString(avatar.getUsername());
+            msg.writeString(avatar.getFigure());
         }
+    }
 
-        return msg;
+    @Override
+    public void dispose() {
+        this.requests.clear();
     }
 }
--- src/main/java/com/cometproject/server/network/messages/outgoing/messenger/MessengerConfigMessageComposer.java
+++ src/main/java/com/cometproject/server/network/messages/outgoing/messenger/MessengerConfigMessageComposer.java
@@ -1,19 +1,23 @@
 package com.cometproject.server.network.messages.outgoing.messenger;
 
+import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.network.messages.headers.Composers;
 import com.cometproject.server.network.messages.types.Composer;
 
 
-public class MessengerConfigMessageComposer {
-    public static Composer compose() {
-        Composer msg = new Composer(Composers.LoadFriendsMessageComposer);
+public class MessengerConfigMessageComposer extends MessageComposer {
 
+    @Override
+    public short getId() {
+        return Composers.LoadFriendsMessageComposer;
+    }
+
+    @Override
+    public void compose(Composer msg) {
         msg.writeInt(1100); // TODO: put this stuff in static config somewhere :P
         msg.writeInt(300);
         msg.writeInt(800);
         msg.writeInt(1100);
         msg.writeInt(0);
-
-        return msg;
     }
 }
