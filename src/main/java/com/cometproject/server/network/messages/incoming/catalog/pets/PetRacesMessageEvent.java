c683d3345265db76ed0c237e9cc165d4fb01391b
Leon Hartley <lhartley97@gmail.com>
2015-03-08 17:12:28 +0000

    Fix pet races error

--- src/main/java/com/cometproject/server/network/messages/incoming/catalog/pets/PetRacesMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/catalog/pets/PetRacesMessageEvent.java
@@ -6,6 +6,7 @@ import com.cometproject.server.network.messages.incoming.IEvent;
 import com.cometproject.server.network.messages.outgoing.catalog.pets.PetRacesMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
 import com.cometproject.server.network.sessions.Session;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.List;
 
@@ -13,9 +14,14 @@ import java.util.List;
 public class PetRacesMessageEvent implements IEvent {
     @Override
     public void handle(Session client, Event msg) {
-        String petRace = msg.readString();
-        int raceId = Integer.parseInt(petRace.split("a0 pet")[1]);
+        final String petRace = msg.readString();
+        final String[] splitRace = petRace.split("a0 pet");
 
+        if(splitRace.length < 2 || !StringUtils.isNumeric(splitRace[1])) {
+            return;
+        }
+
+        int raceId = Integer.parseInt(splitRace[1]);
         List<PetRace> races = PetManager.getInstance().getRacesByRaceId(raceId);
 
         client.send(new PetRacesMessageComposer(petRace, races));
