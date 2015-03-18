b1912b255d51e8931601ce981c0bd2184675ebd8
Leon Hartley <lhartley97@gmail.com>
2015-03-18 22:19:08 +0000

    Clear group favourite

---
+++ src/main/java/com/cometproject/server/network/messages/incoming/group/favourite/ClearFavouriteGroupMessageEvent.java
@@ -0,0 +1,23 @@
+package com.cometproject.server.network.messages.incoming.group.favourite;
+
+import com.cometproject.server.network.messages.incoming.IEvent;
+import com.cometproject.server.network.messages.outgoing.group.UpdateFavouriteGroupMessageComposer;
+import com.cometproject.server.network.messages.outgoing.room.avatar.AvatarsMessageComposer;
+import com.cometproject.server.network.messages.outgoing.room.avatar.LeaveRoomMessageComposer;
+import com.cometproject.server.network.messages.types.Event;
+import com.cometproject.server.network.sessions.Session;
+
+public class ClearFavouriteGroupMessageEvent implements IEvent {
+    @Override
+    public void handle(Session client, Event msg) throws Exception {
+        client.getPlayer().getData().setFavouriteGroup(0);
+        client.getPlayer().getData().save();
+
+        if (client.getPlayer().getEntity() != null) {
+            client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new LeaveRoomMessageComposer(client.getPlayer().getEntity().getId()));
+            client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new AvatarsMessageComposer(client.getPlayer().getEntity()));
+        }
+
+        client.send(new UpdateFavouriteGroupMessageComposer(client.getPlayer().getId()));
+    }
+}
