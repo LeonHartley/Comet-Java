0388ca3bcf0d064fceb505b1297fbf25f26ceb2e
Leon Hartley <lhartley97@gmail.com>
2015-03-20 15:14:29 +0000

    Test guide tool stuff

---
+++ src/main/java/com/cometproject/server/network/messages/incoming/help/tool/OpenGuideToolMessageEvent.java
@@ -0,0 +1,38 @@
+package com.cometproject.server.network.messages.incoming.help.tool;
+
+import com.cometproject.server.network.messages.composers.MessageComposer;
+import com.cometproject.server.network.messages.incoming.IEvent;
+import com.cometproject.server.network.messages.types.Composer;
+import com.cometproject.server.network.messages.types.Event;
+import com.cometproject.server.network.sessions.Session;
+
+public class OpenGuideToolMessageEvent implements IEvent {
+    @Override
+    public void handle(Session client, Event msg) throws Exception {
+        final boolean onDuty = msg.readBoolean();
+
+        final boolean handleTourRequests = msg.readBoolean();
+        final boolean handleHelpRequests = msg.readBoolean();
+        final boolean handleBullyRequests = msg.readBoolean();
+
+        if (onDuty) {
+            // we're on duty!!
+        }
+
+        // 2200
+        client.send(new MessageComposer() {
+            @Override
+            public short getId() {
+                return 2200;
+            }
+
+            @Override
+            public void compose(Composer msg) {
+                msg.writeBoolean(onDuty);
+                msg.writeInt(onDuty ? 1 : 0);
+                msg.writeInt(0);
+                msg.writeInt(0);
+            }
+        });
+    }
+}
