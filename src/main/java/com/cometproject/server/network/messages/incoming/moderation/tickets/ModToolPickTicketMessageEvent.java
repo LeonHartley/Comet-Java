815364d37d59dd15ae6e499c9ed84710a7971699
Leon Hartley <lhartley97@gmail.com>
2015-03-25 18:57:02 +0000

    Alert if ticket was already picked.

--- src/main/java/com/cometproject/server/network/messages/incoming/moderation/tickets/ModToolPickTicketMessageEvent.java
+++ src/main/java/com/cometproject/server/network/messages/incoming/moderation/tickets/ModToolPickTicketMessageEvent.java
@@ -4,6 +4,7 @@ import com.cometproject.server.game.moderation.ModerationManager;
 import com.cometproject.server.game.moderation.types.tickets.HelpTicket;
 import com.cometproject.server.game.moderation.types.tickets.HelpTicketState;
 import com.cometproject.server.network.messages.incoming.IEvent;
+import com.cometproject.server.network.messages.outgoing.notification.AlertMessageComposer;
 import com.cometproject.server.network.messages.types.Event;
 import com.cometproject.server.network.sessions.Session;
 
@@ -20,8 +21,10 @@ public class ModToolPickTicketMessageEvent implements IEvent {
 
         final HelpTicket helpTicket = ModerationManager.getInstance().getTicket(ticketId);
 
-        if(helpTicket == null || helpTicket.getModeratorId() != 0) {
-            // Doesn't exist or already picked!
+        if(helpTicket == null) {
+            return;
+        } else if(helpTicket.getModeratorId() != 0) {
+            client.send(new AlertMessageComposer("This ticket has already been picked by another moderator."));
             return;
         }
 
