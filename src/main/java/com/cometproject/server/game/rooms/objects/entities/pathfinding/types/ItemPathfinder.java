2269f88dacf9abd409b98dc71ca4fbe92808c9ad
Leon Hartley <lhartley97@gmail.com>
2015-04-10 18:51:17 +0100

    Revert "start logging changes"
    This reverts commit d6b3676c94560f3a5c5712b46eb11a69719cca65.

--- src/main/java/com/cometproject/server/game/rooms/objects/entities/pathfinding/types/ItemPathfinder.java
+++ src/main/java/com/cometproject/server/game/rooms/objects/entities/pathfinding/types/ItemPathfinder.java
@@ -1,15 +1,4 @@
 package com.cometproject.server.game.rooms.objects.entities.pathfinding.types;
 
-import com.cometproject.server.game.rooms.objects.entities.pathfinding.Pathfinder;
-
-public class ItemPathfinder extends Pathfinder {
-    private static ItemPathfinder pathfinderInstance;
-
-    public static ItemPathfinder getInstance() {
-        if(pathfinderInstance == null) {
-            pathfinderInstance = new ItemPathfinder();
-        }
-
-        return pathfinderInstance;
-    }
+public class ItemPathfinder {
 }
