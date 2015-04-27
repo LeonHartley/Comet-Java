b60e28fd9a899af7521ab2296d3849558ceb1654
Leon Hartley <lhartley97@gmail.com>
2015-04-27 17:10:52 +0100

    Remove sysout

--- Comet-Server/src/main/java/com/cometproject/server/game/players/components/PermissionComponent.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/players/components/PermissionComponent.java
@@ -31,8 +31,6 @@ public class PermissionComponent implements PlayerComponent {
     }
 
     public boolean hasCommand(String key) {
-        System.out.println(key);
-
         if (PermissionsManager.getInstance().getCommands().containsKey(key)) {
             CommandPermission permission = PermissionsManager.getInstance().getCommands().get(key);
 
