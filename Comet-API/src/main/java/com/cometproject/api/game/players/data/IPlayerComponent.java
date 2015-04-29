9d07f229eade4f5b194c27a01f5fbab469aa4b32
Leon Hartley <lhartley97@gmail.com>
2015-04-29 15:12:46 +0100

    Clean up

--- Comet-API/src/main/java/com/cometproject/api/game/players/data/IPlayerComponent.java
+++ Comet-API/src/main/java/com/cometproject/api/game/players/data/IPlayerComponent.java
@@ -3,6 +3,7 @@ package com.cometproject.api.game.players.data;
 import com.cometproject.api.game.players.IPlayer;
 
 public interface IPlayerComponent {
-    public IPlayer getPlayer();
-    public void dispose();
+    IPlayer getPlayer();
+
+    void dispose();
 }
