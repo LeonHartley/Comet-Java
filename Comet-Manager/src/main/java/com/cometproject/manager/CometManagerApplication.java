6ba1b252046e034fb3e78396e2e89c2ed9790dd5
Leon Hartley <lhartley97@gmail.com>
2015-04-29 18:27:50 +0100

    Start comet manager

---
+++ Comet-Manager/src/main/java/com/cometproject/manager/CometManagerApplication.java
@@ -0,0 +1,15 @@
+package com.cometproject.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CometManagerApplication.class, args);
+    }
+
+    public CometManagerApplication() {
+
+    }
+}
---
+++ Comet-Manager/src/main/java/com/cometproject/manager/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.cometproject.manager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getIndex(HttpServletRequest request) {
+
+
+        return new ModelAndView("index");
+    }
+}
