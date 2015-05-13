47a83a6dded19e84a119480f177295d6d80ea29f
Leon Hartley <lhartley97@gmail.com>
2015-05-13 19:30:36 +0100

    Log slow packets

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/types/tasks/MessageEventTask.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/types/tasks/MessageEventTask.java
@@ -1,5 +1,6 @@
 package com.cometproject.server.network.messages.types.tasks;
 
+import com.cometproject.server.boot.Comet;
 import com.cometproject.server.network.messages.incoming.Event;
 import com.cometproject.server.network.messages.types.MessageEvent;
 import com.cometproject.server.network.sessions.Session;
@@ -13,22 +14,31 @@ public class MessageEventTask implements CometTask {
     private Session session;
     private MessageEvent messageEventData;
 
+    private long start;
+
     public MessageEventTask(Event messageEvent, Session session, MessageEvent messageEventData) {
         this.messageEvent = messageEvent;
         this.session = session;
         this.messageEventData = messageEventData;
+
+        this.start = System.currentTimeMillis();
     }
 
     @Override
     public void run() {
         try {
-            final long start = System.currentTimeMillis();
-
             log.debug("Started packet process for packet: [" + this.messageEvent.getClass().getSimpleName() + "][" + messageEventData.getId() + "]");
-
             this.messageEvent.handle(this.session, this.messageEventData);
 
-            log.debug("Finished packet process for packet: [" + this.messageEvent.getClass().getSimpleName() + "][" + messageEventData.getId() + "] in " + ((System.currentTimeMillis() - start)) + "ms");
+            long timeTakenSinceCreation = ((System.currentTimeMillis() - this.start));
+
+            if (timeTakenSinceCreation >= 50) {
+                if (session.getPlayer() != null && session.getPlayer().getData() != null)
+                    log.warn("[" + this.messageEvent.getClass().getSimpleName() + "][" + messageEventData.getId() + "][" + session.getPlayer().getId() + "][" + session.getPlayer().getData().getUsername() + "] Packet took " + timeTakenSinceCreation + "ms to execute");
+                else
+                    log.warn("[" + this.messageEvent.getClass().getSimpleName() + "][" + messageEventData.getId() + "] Packet took " +  timeTakenSinceCreation + "ms to execute");
+            }
+            log.debug("Finished packet process for packet: [" + this.messageEvent.getClass().getSimpleName() + "][" + messageEventData.getId() + "] in " + timeTakenSinceCreation +"ms");
 
         } catch (Exception e) {
             if (this.session.getLogger() != null)
