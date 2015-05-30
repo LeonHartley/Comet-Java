768bceaa71909b72d49edbe9a554325002596aa4
Leon Hartley <lhartley97@gmail.com>
2015-05-29 20:04:15 -0700

    Unseen items changes

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/catalog/UnseenItemsMessageComposer.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/catalog/UnseenItemsMessageComposer.java
@@ -5,15 +5,33 @@ import com.cometproject.server.game.players.components.types.inventory.Inventory
 import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.network.messages.headers.Composers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class UnseenItemsMessageComposer extends MessageComposer {
 
-    private final List<InventoryItem> inventoryItems;
+    private final List<Integer> tabs;
+    private final List<Object> newItems;
 
-    public UnseenItemsMessageComposer(final List<InventoryItem> items) {
-        this.inventoryItems = items;
+    public UnseenItemsMessageComposer(final List<Integer> tabs, final List<Object> items) {
+        this.tabs = tabs;
+        this.newItems = items;
+    }
+
+    public UnseenItemsMessageComposer(final List<InventoryItem> inventoryItems) {
+        this.tabs = new ArrayList<>();
+        this.newItems = new ArrayList<>();
+
+        for(InventoryItem inventoryItem : inventoryItems) {
+            if(!inventoryItem.getDefinition().getType().toLowerCase().equals("s") && !this.tabs.contains(2)) {
+                this.tabs.add(2);
+            } else {
+                if(!this.tabs.contains(1)) {
+                    this.tabs.add(1);
+                }
+            }
+        }
     }
 
     @Override
@@ -23,24 +41,25 @@ public class UnseenItemsMessageComposer extends MessageComposer {
 
     @Override
     public void compose(IComposer msg) {
-        int inventoryTab = 1;
-        for (InventoryItem item : this.inventoryItems) {
-            if (!item.getDefinition().getType().equals("s"))
-                inventoryTab = 2;
+        msg.writeInt(tabs.size());
+
+        for(int tabId : this.tabs) {
+            msg.writeInt(tabId);
         }
 
-        msg.writeInt(1); // count of tabs that need updating
-        msg.writeInt(inventoryTab); // tab id
+        msg.writeInt(this.newItems.size());
 
-        msg.writeInt(this.inventoryItems.size());
-
-        for (InventoryItem item : this.inventoryItems) {
-            msg.writeInt(item.getId());
+        for(Object obj : this.newItems) {
+            if(obj instanceof Integer) {
+                msg.writeInt((Integer) obj);
+            } else if(obj instanceof String) {
+                msg.writeString(obj);
+            }
         }
     }
 
     @Override
     public void dispose() {
-        this.inventoryItems.clear();
+        this.newItems.clear();
     }
 }
