40670f67a85a81ff4ac41f9728c1084cdda34f83
Leon Hartley <lhartley97@gmail.com>
2015-06-15 16:29:32 +0100

    Camera!

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/user/achievements/AchievementProgressMessageComposer.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/user/achievements/AchievementProgressMessageComposer.java
@@ -10,12 +10,10 @@ import com.cometproject.server.network.messages.headers.Composers;
 public class AchievementProgressMessageComposer extends MessageComposer {
 
     private final AchievementGroup achievementGroup;
-    private final Achievement achievement;
     private final AchievementProgress achievementProgress;
 
-    public AchievementProgressMessageComposer(AchievementProgress achievementProgress, Achievement achievement, AchievementGroup achievementGroup) {
+    public AchievementProgressMessageComposer(AchievementProgress achievementProgress,AchievementGroup achievementGroup) {
         this.achievementProgress = achievementProgress == null ? null : new AchievementProgress(achievementProgress.getLevel(), achievementProgress.getProgress());
-        this.achievement = achievement;
         this.achievementGroup = achievementGroup;
     }
 
@@ -26,15 +24,15 @@ public class AchievementProgressMessageComposer extends MessageComposer {
 
     @Override
     public void compose(IComposer msg) {
-        msg.writeInt(this.achievement.getId());
-        msg.writeInt(this.achievement.getLevel());
-        msg.writeString(this.achievementGroup.getGroupName() + this.achievement.getLevel());
-        msg.writeInt(this.achievement.getProgressNeeded());
-        msg.writeInt(this.achievement.getProgressNeeded());
-        msg.writeInt(this.achievement.getRewardActivityPoints());
+        msg.writeInt(this.achievementGroup.getAchievement(achievementProgress.getLevel()).getId());
+        msg.writeInt(this.achievementGroup.getAchievement(achievementProgress.getLevel()).getLevel());
+        msg.writeString(this.achievementGroup.getGroupName() + this.achievementGroup.getAchievement(achievementProgress.getLevel()).getLevel());
+        msg.writeInt(this.achievementGroup.getAchievement(achievementProgress.getLevel()).getProgressNeeded());
+        msg.writeInt(this.achievementGroup.getAchievement(achievementProgress.getLevel()).getProgressNeeded());
+        msg.writeInt(this.achievementGroup.getAchievement(achievementProgress.getLevel()).getRewardActivityPoints());
         msg.writeInt(0);
-        msg.writeInt(this.achievementProgress != null ? this.achievementProgress.getProgress() : 0);
-        msg.writeBoolean(this.achievementProgress != null && this.achievementProgress.getLevel() >= this.achievementGroup.getAchievements().size());
+        msg.writeInt(this.achievementProgress.getProgress());
+        msg.writeBoolean(this.achievementProgress.getLevel() >= this.achievementGroup.getAchievements().size());
         msg.writeString(this.achievementGroup.getCategory().toString().toLowerCase());
         msg.writeString("");
         msg.writeInt(this.achievementGroup.getAchievements().size());
--- Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/user/achievements/AchievementUnlockedMessageComposer.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/user/achievements/AchievementUnlockedMessageComposer.java
@@ -2,19 +2,23 @@ package com.cometproject.server.network.messages.outgoing.user.achievements;
 
 import com.cometproject.api.networking.messages.IComposer;
 import com.cometproject.server.game.achievements.AchievementGroup;
+import com.cometproject.server.game.achievements.types.Achievement;
 import com.cometproject.server.game.players.components.AchievementComponent;
 import com.cometproject.server.game.players.components.types.achievements.AchievementProgress;
 import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.network.messages.headers.Composers;
 
 public class AchievementUnlockedMessageComposer extends MessageComposer {
+    private final String achievementCategory;
+    private final String achievementName;
     private final int achievementLevel;
+    private final Achievement currentAchievement;
 
-    private final AchievementGroup achievementGroup;
-
-    public AchievementUnlockedMessageComposer(int achievementLevel, AchievementGroup achievementGroup) {
+    public AchievementUnlockedMessageComposer(final String achievementCategory, String achievementName, int achievementLevel, Achievement currentAchievement) {
+        this.achievementCategory = achievementCategory;
+        this.achievementName = achievementName;
         this.achievementLevel = achievementLevel;
-        this.achievementGroup = achievementGroup;
+        this.currentAchievement = currentAchievement;
     }
 
     @Override
@@ -24,17 +28,17 @@ public class AchievementUnlockedMessageComposer extends MessageComposer {
 
     @Override
     public void compose(IComposer msg) {
-        msg.writeInt(achievementGroup.getAchievement(achievementLevel).getId());
+        msg.writeInt(currentAchievement.getId());
         msg.writeInt(achievementLevel);
         msg.writeInt(144); // TODO: Find out what this is.
-        msg.writeString(achievementGroup.getGroupName() + achievementLevel);
-        msg.writeInt(achievementGroup.getAchievement(achievementLevel).getRewardAchievement());
-        msg.writeInt(achievementGroup.getAchievement(achievementLevel).getRewardActivityPoints());
+        msg.writeString(achievementName + achievementLevel);
+        msg.writeInt(currentAchievement.getRewardAchievement());
+        msg.writeInt(currentAchievement.getRewardActivityPoints());
         msg.writeInt(0);
         msg.writeInt(10);
         msg.writeInt(21);
-        msg.writeString(achievementLevel > 1 ? achievementGroup.getGroupName() + (achievementLevel - 1) : "");
-        msg.writeString(achievementGroup.getCategory().toString().toLowerCase());
+        msg.writeString(achievementLevel > 1 ? achievementName + (achievementLevel - 1) : "");
+        msg.writeString(achievementCategory.toLowerCase());
         msg.writeBoolean(false);
     }
 }
