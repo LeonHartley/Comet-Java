34d0cb604a1fdb3401d4133243f05ed22b549841
Leon Hartley <lhartley97@gmail.com>
2015-07-28 21:15:01 +0100

    Forum permission updates

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/group/forums/GroupForumDataMessageComposer.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/group/forums/GroupForumDataMessageComposer.java
@@ -2,6 +2,7 @@ package com.cometproject.server.network.messages.outgoing.group.forums;
 
 import com.cometproject.api.networking.messages.IComposer;
 import com.cometproject.server.game.groups.types.Group;
+import com.cometproject.server.game.groups.types.components.forum.settings.ForumSettings;
 import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.protocol.headers.Composers;
 
@@ -9,7 +10,7 @@ public class GroupForumDataMessageComposer extends MessageComposer {
     private final Group group;
     private final int playerId;
 
-    public GroupForumDataMessageComposer(final Group group, int playerId) {
+    public  GroupForumDataMessageComposer(final Group group, int playerId) {
         this.group = group;
         this.playerId = playerId;
     }
@@ -25,24 +26,31 @@ public class GroupForumDataMessageComposer extends MessageComposer {
         msg.writeString(group.getData().getTitle());
         msg.writeString(group.getData().getDescription());
         msg.writeString(group.getData().getBadge());
+
         msg.writeInt(0);//total threads
         msg.writeInt(0);//leaderboard score
         msg.writeInt(0);//total messages
         msg.writeInt(0);//unread messages
+
         msg.writeInt(0);//last message id
         msg.writeInt(0);//last message author id
         msg.writeString("");//last message author name
         msg.writeInt(0);//last message time
-        msg.writeInt(1);// read permissions
-        msg.writeInt(1);// write permissions
-        msg.writeInt(1);// post thread permissions
-        msg.writeInt(1);// can moderate
+
+        final ForumSettings forumSettings = this.group.getForumComponent().getForumSettings();
+
+        msg.writeInt(forumSettings.getReadPermission().getPermissionId());
+        msg.writeInt(forumSettings.getPostPermission().getPermissionId());
+        msg.writeInt(forumSettings.getStartThreadsPermission().getPermissionId());
+        msg.writeInt(forumSettings.getModeratePermission().getPermissionId());
+
         msg.writeString("");//1
         msg.writeString("");//2
         msg.writeString("");//3
         msg.writeString("");//4
         msg.writeString("");//??
+
         msg.writeBoolean(group.getData().getOwnerId() == playerId);
-        msg.writeBoolean(true);
+        msg.writeBoolean(false);
     }
 }
