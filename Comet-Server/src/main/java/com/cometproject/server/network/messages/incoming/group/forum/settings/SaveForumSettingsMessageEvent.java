a73681f19889d9eed0dc1bb04213f6508761c519
Leon Hartley <lhartley97@gmail.com>
2015-07-28 21:15:01 +0100

    Forum permission updates

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/group/forum/settings/SaveForumSettingsMessageEvent.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/group/forum/settings/SaveForumSettingsMessageEvent.java
@@ -1,16 +1,48 @@
 package com.cometproject.server.network.messages.incoming.group.forum.settings;
 
+import com.cometproject.server.game.groups.GroupManager;
+import com.cometproject.server.game.groups.types.Group;
+import com.cometproject.server.game.groups.types.components.forum.settings.ForumPermission;
+import com.cometproject.server.game.groups.types.components.forum.settings.ForumSettings;
 import com.cometproject.server.network.messages.incoming.Event;
+import com.cometproject.server.network.messages.outgoing.group.forums.GroupForumDataMessageComposer;
 import com.cometproject.server.protocol.messages.MessageEvent;
 import com.cometproject.server.network.sessions.Session;
 
 public class SaveForumSettingsMessageEvent implements Event {
     @Override
     public void handle(Session client, MessageEvent msg) throws Exception {
-        int groupId = msg.readInt();
+        final int groupId = msg.readInt();
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(msg.readInt());
+        int readForum = msg.readInt();
+        int postMessages = msg.readInt();
+        int startThreads = msg.readInt();
+        int moderate = msg.readInt();
+
+        final ForumPermission whoCanReadForum = ForumPermission.getById(readForum);
+        final ForumPermission whoCanPostMessages = ForumPermission.getById(postMessages);
+        final ForumPermission whoCanStartThreads = ForumPermission.getById(startThreads);
+        final ForumPermission whoCanModerate = ForumPermission.getById(moderate);
+
+        Group group = GroupManager.getInstance().get(groupId);
+
+        if(group == null || group.getData().getOwnerId() != client.getPlayer().getId()) {
+            return;
         }
+
+        if(!group.getData().hasForum()) {
+            return;
+        }
+
+        ForumSettings forumSettings = group.getForumComponent().getForumSettings();
+
+        forumSettings.setReadPermission(whoCanReadForum);
+        forumSettings.setModeratePermission(whoCanModerate);
+        forumSettings.setStartThreadsPermission(whoCanStartThreads);
+        forumSettings.setPostPermission(whoCanPostMessages);
+
+        forumSettings.save();
+
+        client.send(new GroupForumDataMessageComposer(group, client.getPlayer().getId()));
     }
 }
