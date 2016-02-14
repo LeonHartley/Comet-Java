34e364f74a72c4123e3adfa012ffd3059313009b
Leon Hartley <lhartley97@gmail.com>
2016-02-14 21:06:51 +0000

    Cleanup

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/pets/horse/RemoveHorseSaddleMessageEvent.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/pets/horse/RemoveHorseSaddleMessageEvent.java
@@ -3,7 +3,6 @@ package com.cometproject.server.network.messages.incoming.room.pets.horse;
 import com.cometproject.server.game.items.ItemManager;
 import com.cometproject.server.game.players.components.types.inventory.InventoryItem;
 import com.cometproject.server.game.rooms.objects.entities.types.PetEntity;
-import com.cometproject.server.game.rooms.objects.items.RoomItemFloor;
 import com.cometproject.server.game.rooms.types.Room;
 import com.cometproject.server.network.messages.incoming.Event;
 import com.cometproject.server.network.messages.outgoing.catalog.UnseenItemsMessageComposer;
@@ -13,16 +12,13 @@ import com.cometproject.server.network.sessions.Session;
 import com.cometproject.server.protocol.messages.MessageEvent;
 import com.cometproject.server.storage.queries.items.ItemDao;
 import com.google.common.collect.Lists;
-import org.apache.commons.collections4.ListUtils;
-
-import java.util.Collections;
 
 public class RemoveHorseSaddleMessageEvent implements Event {
     @Override
     public void handle(Session client, MessageEvent msg) throws Exception {
         final int petId = msg.readInt();
 
-        if(client.getPlayer().getEntity() == null || client.getPlayer().getEntity().getRoom() == null) {
+        if (client.getPlayer().getEntity() == null || client.getPlayer().getEntity().getRoom() == null) {
             return;
         }
 
@@ -30,11 +26,11 @@ public class RemoveHorseSaddleMessageEvent implements Event {
 
         PetEntity petEntity = room.getEntities().getEntityByPetId(petId);
 
-        if(petEntity == null || petEntity.getData().getOwnerId() != client.getPlayer().getId()) {
+        if (petEntity == null || petEntity.getData().getOwnerId() != client.getPlayer().getId()) {
             return;
         }
 
-        if(ItemManager.getInstance().getSaddleId() != null) {
+        if (ItemManager.getInstance().getSaddleId() != null) {
             petEntity.getData().setSaddled(false);
             petEntity.getData().saveHorseData();
 
