016c5339908859e0c88a5e6bdfd37130e24f4b75
Leon Hartley <lhartley97@gmail.com>
2016-02-17 10:49:41 +0000

    Allow badges to be given to offline players

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/rewards/GiveBadgeCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/rewards/GiveBadgeCommand.java
@@ -2,8 +2,12 @@ package com.cometproject.server.game.commands.staff.rewards;
 
 import com.cometproject.server.config.Locale;
 import com.cometproject.server.game.commands.ChatCommand;
+import com.cometproject.server.game.players.PlayerManager;
+import com.cometproject.server.game.players.data.PlayerAvatar;
 import com.cometproject.server.network.NetworkManager;
 import com.cometproject.server.network.sessions.Session;
+import com.cometproject.server.storage.queries.player.PlayerDao;
+import com.cometproject.server.storage.queries.player.inventory.InventoryDao;
 
 
 public class GiveBadgeCommand extends ChatCommand {
@@ -21,7 +25,14 @@ public class GiveBadgeCommand extends ChatCommand {
             session.getPlayer().getInventory().addBadge(badge, true);
             sendNotif(Locale.get("command.givebadge.success").replace("%username%", username).replace("%badge%", badge), client);
         } else {
-            sendNotif(Locale.get("command.givebadge.fail").replace("%username%", username).replace("%badge%", badge), client);
+            int playerId = PlayerDao.getIdByUsername(username);
+
+            if(playerId == 0) {
+                sendNotif(Locale.get("command.givebadge.fail").replace("%username%", username).replace("%badge%", badge), client);
+            } else {
+                InventoryDao.addBadge(badge, playerId);
+                sendNotif(Locale.get("command.givebadge.success").replace("%username%", username).replace("%badge%", badge), client);
+            }
         }
     }
 
