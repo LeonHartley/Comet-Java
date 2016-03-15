4df98fad73e8ceaafa7e46b427298f943ed3a56c
Leon Hartley <lhartley97@gmail.com>
2016-03-15 09:37:34 +0000

    Allow room owners to create quick polls

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/QuickPollCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/QuickPollCommand.java
@@ -2,6 +2,7 @@ package com.cometproject.server.game.commands.staff;
 
 import com.cometproject.server.config.Locale;
 import com.cometproject.server.game.commands.ChatCommand;
+import com.cometproject.server.game.rooms.types.Room;
 import com.cometproject.server.network.sessions.Session;
 
 public class QuickPollCommand extends ChatCommand {
@@ -9,6 +10,14 @@ public class QuickPollCommand extends ChatCommand {
     public void execute(Session client, String[] params) {
         if (params.length == 0) {
             return;
+        }
+
+        final Room room = client.getPlayer().getEntity().getRoom();
+
+        if(!room.getRights().hasRights(client.getPlayer().getId())) {
+            if(!client.getPlayer().getPermissions().getRank().modTool()) {
+                return;
+            }
         }
 
         String question = this.merge(params);
