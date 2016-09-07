6ef9ffd1a6eee567dff6bc762def5204b296bc93
Leon Hartley <lhartley97@gmail.com>
2016-09-07 15:20:44 +0100

    Rewrite structure for SlideObjectBundle
    This allows the movement of multiple items on the same stack via a
    single packet!

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/room/items/SlideObjectBundleMessageComposer.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/outgoing/room/items/SlideObjectBundleMessageComposer.java
@@ -5,6 +5,8 @@ import com.cometproject.server.game.rooms.objects.misc.Position;
 import com.cometproject.server.network.messages.composers.MessageComposer;
 import com.cometproject.server.protocol.headers.Composers;
 
+import java.util.Map;
+
 
 public class SlideObjectBundleMessageComposer extends MessageComposer {
     private final Position from;
@@ -12,6 +14,7 @@ public class SlideObjectBundleMessageComposer extends MessageComposer {
     private final int rollerItemId;
     private final int avatarId;
     private final int itemId;
+    private final Map<Integer, Double> items;
 
     public SlideObjectBundleMessageComposer(Position from, Position to, int rollerItemId, int avatarId, int itemId) {
         this.from = from;
@@ -19,6 +22,17 @@ public class SlideObjectBundleMessageComposer extends MessageComposer {
         this.rollerItemId = rollerItemId;
         this.avatarId = avatarId;
         this.itemId = itemId;
+        this.items = null;
+    }
+
+    public SlideObjectBundleMessageComposer(Position from, Position to, int rollerItemId, int avatarId,
+                                            Map<Integer, Double> items) {
+        this.from = from;
+        this.to = to;
+        this.rollerItemId = rollerItemId;
+        this.avatarId = avatarId;
+        this.itemId = 0;
+        this.items = items;
     }
 
     @Override
@@ -28,12 +42,48 @@ public class SlideObjectBundleMessageComposer extends MessageComposer {
 
     @Override
     public void compose(IComposer msg) {
+        if(this.items == null) {
+            composeLegacy(msg);
+            return;
+        }
+
+        msg.writeInt(from.getX());
+        msg.writeInt(from.getY());
+
+        msg.writeInt(to.getX());
+        msg.writeInt(to.getY());
+
+        msg.writeInt(this.items.size());
+
+        for(Map.Entry<Integer, Double> item : this.items.entrySet()) {
+            msg.writeInt(item.getKey());
+
+            // we want to ensure we slide to the same height as we were previously at.
+            msg.writeDouble(item.getValue());
+            msg.writeDouble(item.getValue());
+        }
+
+        msg.writeInt(this.rollerItemId);
+
+        if(this.avatarId != 0) {
+            // 1 = mv, 2 = std
+            msg.writeInt(2);
+
+            msg.writeInt(this.avatarId);
+            msg.writeDouble(from.getZ());
+            msg.writeDouble(to.getZ());
+        }
+    }
+
+    private void composeLegacy(IComposer msg) {
         final boolean isItem = itemId > 0;
 
         msg.writeInt(from.getX());
         msg.writeInt(from.getY());
+
         msg.writeInt(to.getX());
         msg.writeInt(to.getY());
+
         msg.writeInt(isItem ? 1 : 0);
 
         if (isItem) {
