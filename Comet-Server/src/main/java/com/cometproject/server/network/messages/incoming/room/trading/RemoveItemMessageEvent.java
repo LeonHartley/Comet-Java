6fb6a00f94a76a1c7209d5860f39f52488575585
ryanleet <ryanleetweb@gmail.com>
2016-09-24 08:41:22 +0200

    Trade bugfixes
    Remove item from the trade window

---
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/trading/RemoveItemMessageEvent.java
@@ -0,0 +1,29 @@
+package com.cometproject.server.network.messages.incoming.room.trading;
+
+import com.cometproject.api.game.players.data.components.inventory.PlayerItem;
+import com.cometproject.server.game.items.ItemManager;
+import com.cometproject.server.game.rooms.types.components.types.Trade;
+import com.cometproject.server.network.messages.incoming.Event;
+import com.cometproject.server.protocol.messages.MessageEvent;
+import com.cometproject.server.network.sessions.Session;
+
+public class RemoveItemMessageEvent implements Event {
+    @Override
+    public void handle(Session client, MessageEvent msg) throws Exception {
+        long itemId = ItemManager.getInstance().getItemIdByVirtualId(msg.readInt());
+        PlayerItem item = client.getPlayer().getInventory().getFloorItem(itemId);
+
+        if (item == null) {
+            item = client.getPlayer().getInventory().getWallItem(itemId);
+
+            if (item == null) {
+                return;
+            }
+        }
+
+        Trade trade = client.getPlayer().getEntity().getRoom().getTrade().get(client.getPlayer().getEntity());
+        if (trade == null) return;
+
+        trade.removeItem(trade.getUserNumber(client.getPlayer().getEntity()), item);
+    }
+}
