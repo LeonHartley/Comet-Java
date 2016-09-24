7fefe20ab57c648c1c12dc5bcf3f22406fa1873b
ryanleet <ryanleetweb@gmail.com>
2016-09-24 09:09:18 +0200

    Update MessageHandler.java

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/MessageHandler.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/MessageHandler.java
@@ -306,8 +306,7 @@ public final class MessageHandler {
         this.getMessages().put(Events.TradingCancelMessageEvent, new CancelTradeMessageEvent());
         this.getMessages().put(Events.TradingModifyMessageEvent, new UnacceptTradeMessageEvent());
         this.getMessages().put(Events.TradingOfferItemMessageEvent, new SendOfferMessageEvent());
-        this.getMessages().put(Events.CancelOfferMessageEvent, new CancelOfferMessageEvent());
-        this.getMessages().put(Events.TradingRemoveItemMessageEvent, new RemoveItemMessageEvent());
+        this.getMessages().put(Events.TradingRemoveItemMessageEvent, new CancelOfferMessageEvent());
         this.getMessages().put(Events.TradingAcceptMessageEvent, new AcceptTradeMessageEvent());
         this.getMessages().put(Events.TradingConfirmMessageEvent, new ConfirmTradeMessageEvent());
         this.getMessages().put(Events.TradingOfferItemsMessageEvent, new TradingOfferItemsMessageEvent());
