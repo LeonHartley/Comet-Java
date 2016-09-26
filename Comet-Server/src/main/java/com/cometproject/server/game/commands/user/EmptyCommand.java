12dd226ba984e8c18e37ed7320faa45fecff8fb6
ryanleet <ryanleetweb@gmail.com>
2016-09-26 05:14:47 +0200

    Update EmptyCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/user/EmptyCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/user/EmptyCommand.java
@@ -14,39 +14,34 @@ import com.cometproject.server.storage.queries.player.inventory.InventoryDao;
 public class EmptyCommand extends ChatCommand {
     @Override
     public void execute(Session client, String[] params) {
-        if (params.length != 1) {
-            client.getPlayer().getInventory().getFloorItems().clear();
-            client.getPlayer().getInventory().getWallItems().clear();
-
-            InventoryDao.clearInventory(client.getPlayer().getId());
-
-            sendNotif(Locale.getOrDefault("command.empty.emptied", "Your inventory was cleared."), client);
+if (params.length != 1) {
+            sendAlert(Locale.getOrDefault("command.empty.confirm", "<b>Warning!</b>\rAre you sure? You are going to delete your Furni, Bots & Pets.\r\rIf you are sure type  <b>:" + Locale.get("command.empty.name")  + " yes</b>"), client);
         } else {
             switch (params[0]) {
                 default:
-
+                    sendAlert(Locale.getOrDefault("command.empty.confirm", "<b>Warning!</b>\rAre you sure? You are going to delete your Furni, Bots & Pets.\r\rIf you are sure type  <b>:" + Locale.get("command.empty.name")  + " yes</b>"), client);
                     break;
 
-                case "pets":
+                case "yes":
+                    client.getPlayer().getInventory().getFloorItems().clear();
+                    client.getPlayer().getInventory().getWallItems().clear();
+
+                    InventoryDao.clearInventory(client.getPlayer().getId());
+
                     PetDao.deletePets(client.getPlayer().getId());
                     client.getPlayer().getPets().clearPets();
 
                     client.send(new PetInventoryMessageComposer(client.getPlayer().getPets().getPets()));
 
-                    sendNotif(Locale.getOrDefault("command.empty.emptied_pets", "Your inventory was cleared."), client);
-                    return;
-
-                case "bots":
                     PlayerBotDao.deleteBots(client.getPlayer().getId());
                     client.getPlayer().getBots().clearBots();
                     client.send(new BotInventoryMessageComposer());
 
-                    sendNotif(Locale.getOrDefault("command.empty.emptied_bots", "Your inventory was cleared."), client);
-                    return;
+                    sendNotif(Locale.getOrDefault("command.empty.emptied", "Your inventory was cleared."), client);
             }
+            
+            client.send(new UpdateInventoryMessageComposer());
         }
-
-        client.send(new UpdateInventoryMessageComposer());
     }
 
     @Override
