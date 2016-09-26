f7e5a534d3a8f08e990e8110cd8a9c5028aba2a1
ryanleet <ryanleetweb@gmail.com>
2016-09-26 03:18:15 +0200

    Add files via upload

---
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/trading/CancelConfirmTradeMessageEvent.java
@@ -0,0 +1,21 @@
+package com.cometproject.server.network.messages.incoming.room.trading;
+
+import com.cometproject.server.game.rooms.types.components.types.Trade;
+import com.cometproject.server.network.messages.incoming.Event;
+import com.cometproject.server.protocol.messages.MessageEvent;
+import com.cometproject.server.network.sessions.Session;
+
+
+public class CancelConfirmTradeMessageEvent implements Event {
+    public void handle(Session client, MessageEvent msg) {
+        if (client.getPlayer().getEntity() == null || client.getPlayer().getEntity().getRoom() == null) return;
+
+        Trade trade = client.getPlayer().getEntity().getRoom().getTrade().get(client.getPlayer().getEntity());
+
+        if (trade == null) {
+            return;
+        }
+
+        trade.cancel(client.getPlayer().getId());
+    }
+}
