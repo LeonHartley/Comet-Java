ef91d20e3c075f4f860ba297769a9994465ebd84
ryanleet <ryanleetweb@gmail.com>
2016-09-26 21:03:48 +0200

    Update KissCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/gimmicks/KissCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/gimmicks/KissCommand.java
@@ -12,14 +12,20 @@ import com.cometproject.server.network.sessions.Session;
 public class KissCommand extends ChatCommand {
     @Override
     public void execute(Session client, String[] params) {
-        if (params.length != 1) return;
-
+        if (params.length != 1) {
+            sendNotif(Locale.getOrDefault("command.user.invalid", "Username is invalid!"), client);
+            return;
+        }
+            
         String kissedPlayer = params[0];
 
         RoomEntity entity = client.getPlayer().getEntity().getRoom().getEntities().getEntityByName(kissedPlayer, RoomEntityType.PLAYER);
 
-        if (entity == null) return;
-
+        if (entity == null) {
+            sendNotif(Locale.getOrDefault("command.user.offline", "This user is offline!"), client);
+            return;
+        }
+            
         client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new WhisperMessageComposer(client.getPlayer().getEntity().getId(), "* " + client.getPlayer().getData().getUsername() + " " + Locale.getOrDefault("command.kiss.word", "snogs") + " " + entity.getUsername() + " *", 34));
         client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new ActionMessageComposer(client.getPlayer().getEntity().getId(), 2));
     }
