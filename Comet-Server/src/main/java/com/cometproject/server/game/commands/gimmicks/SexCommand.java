eebcfa55253b5c7c17524f1c3686f4e902ad945b
ryanleet <ryanleetweb@gmail.com>
2016-09-26 21:08:09 +0200

    Update SexCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/gimmicks/SexCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/gimmicks/SexCommand.java
@@ -13,13 +13,19 @@ public class SexCommand extends ChatCommand {
 
     @Override
     public void execute(Session client, String[] params) {
-        if (params.length != 1) return;
+        if (params.length != 1) {
+            sendNotif(Locale.getOrDefault("command.user.invalid", "Invalid username!"), client);
+            return;
+        }
 
         String sexedPlayer = params[0];
 
         RoomEntity entity = client.getPlayer().getEntity().getRoom().getEntities().getEntityByName(sexedPlayer, RoomEntityType.PLAYER);
 
-        if (entity == null) return;
+        if (entity == null) {
+            sendNotif(Locale.getOrDefault("command.user.notinroom", "This user is not in a room."), client);
+            return;
+        }
 
         client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new WhisperMessageComposer(client.getPlayer().getEntity().getId(), "* " + client.getPlayer().getData().getUsername() + " sexed " + entity.getUsername() + " *", 34));
         client.getPlayer().getEntity().getRoom().getEntities().broadcastMessage(new ActionMessageComposer(entity.getId(), 7));
