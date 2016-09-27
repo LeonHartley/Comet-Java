88a6e908cb0cfeeaa35092a8cd8ebb68bc9ac131
ryanleet <ryanleetweb@gmail.com>
2016-09-28 01:01:49 +0200

    Update HandItemCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/vip/HandItemCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/vip/HandItemCommand.java
@@ -27,6 +27,11 @@ public class HandItemCommand extends ChatCommand {
     public String getPermission() {
         return Locale.get("handitem_command");
     }
+    
+    @Override
+    public String getParameter() {
+        return Locale.getOrDefault("command.number.parameter", "%number%");
+    }
 
     @Override
     public String getDescription() {
