30fb1421589ac6fcf9af2775abf0c21944923701
ryanleet <ryanleetweb@gmail.com>
2016-09-28 01:40:31 +0200

    Update MuteCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/muting/MuteCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/muting/MuteCommand.java
@@ -34,6 +34,11 @@ public class MuteCommand extends ChatCommand {
     public String getPermission() {
         return "mute_command";
     }
+    
+    @Override
+    public String getParameter() {
+        return Locale.getOrDefault("command.parameter.username", "%username%");
+    }
 
     @Override
     public String getDescription() {
