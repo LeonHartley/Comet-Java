0ab92a0a861e3b560edd54c5019688980f1130aa
ryanleet <ryanleetweb@gmail.com>
2016-09-28 02:02:23 +0200

    Update FastProcessCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/development/FastProcessCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/development/FastProcessCommand.java
@@ -20,6 +20,11 @@ public class FastProcessCommand extends ChatCommand {
     public String getPermission() {
         return "dev";
     }
+    
+    @Override
+    public String getParameter() {
+        return "";
+    }
 
     @Override
     public String getDescription() {
