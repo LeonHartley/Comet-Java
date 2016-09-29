3e81361264244ce972715c15caf7ba625614ebb8
ryanleet <ryanleetweb@gmail.com>
2016-09-29 03:36:24 +0200

    Update HandItemCommand.java

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/vip/HandItemCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/vip/HandItemCommand.java
@@ -17,7 +17,6 @@ public class HandItemCommand extends ChatCommand {
 
             if (handItem > 0) {
                 client.getPlayer().getEntity().carryItem(handItem, false);
-                isExecuted(client);
             }
         } catch (Exception e) {
             sendNotif(Locale.getOrDefault("command.handitem.invalid", "Please, use numbers only!"), client);
