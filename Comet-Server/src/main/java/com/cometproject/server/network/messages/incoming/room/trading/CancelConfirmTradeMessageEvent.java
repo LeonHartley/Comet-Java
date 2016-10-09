5d8ace1c99d965f070c992111e9f15535472c8b3
ryanleet <ryanleetweb@gmail.com>
2016-10-09 22:32:32 +0200

    Update CancelConfirmTradeMessageEvent.java

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/trading/CancelConfirmTradeMessageEvent.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/trading/CancelConfirmTradeMessageEvent.java
@@ -16,6 +16,7 @@ public class CancelConfirmTradeMessageEvent implements Event {
             return;
         }
 
-        trade.cancel(client.getPlayer().getId());
+        trade.cancel(trade.getUser1().getPlayerId());
+        trade.cancel(trade.getUser2().getPlayerId());
     }
 }
