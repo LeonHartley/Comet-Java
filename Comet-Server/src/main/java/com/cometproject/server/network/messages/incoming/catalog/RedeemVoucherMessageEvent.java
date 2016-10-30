b219421629ae543d1482cb9295da477ff2091b4c
Leon Hartley <lhartley97@gmail.com>
2016-10-30 10:08:59 -0700

    Room bundle voucher reward

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/catalog/RedeemVoucherMessageEvent.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/catalog/RedeemVoucherMessageEvent.java
@@ -1,13 +1,24 @@
 package com.cometproject.server.network.messages.incoming.catalog;
 
+import com.cometproject.server.boot.Comet;
 import com.cometproject.server.config.Locale;
 import com.cometproject.server.game.catalog.types.Voucher;
 import com.cometproject.server.game.catalog.types.VoucherStatus;
+import com.cometproject.server.game.rooms.RoomManager;
+import com.cometproject.server.game.rooms.bundles.RoomBundleManager;
+import com.cometproject.server.game.rooms.bundles.types.RoomBundle;
+import com.cometproject.server.game.rooms.bundles.types.RoomBundleItem;
 import com.cometproject.server.network.messages.incoming.Event;
+import com.cometproject.server.network.messages.outgoing.catalog.BoughtItemMessageComposer;
 import com.cometproject.server.network.messages.outgoing.notification.AdvancedAlertMessageComposer;
+import com.cometproject.server.network.messages.outgoing.notification.MotdNotificationMessageComposer;
+import com.cometproject.server.network.messages.outgoing.room.engine.RoomForwardMessageComposer;
+import com.cometproject.server.network.messages.outgoing.room.settings.EnforceRoomCategoryMessageComposer;
 import com.cometproject.server.network.sessions.Session;
 import com.cometproject.server.protocol.messages.MessageEvent;
 import com.cometproject.server.storage.queries.catalog.VoucherDao;
+import com.cometproject.server.storage.queries.items.ItemDao;
+import com.cometproject.server.storage.queries.rooms.RoomItemDao;
 import org.apache.commons.lang.NumberUtils;
 import org.apache.commons.lang.StringUtils;
 
@@ -50,8 +61,37 @@ public class RedeemVoucherMessageEvent implements Event {
 
                 client.getPlayer().getData().increaseCredits(coinAmount);
                 client.send(new AdvancedAlertMessageComposer(Locale.get("command.coins.title"), Locale.get("command.coins.received").replace("%amount%", String.valueOf(coinAmount))));
+                break;
             }
-            break;
+
+            case ROOM_BUNDLE: {
+                RoomBundle roomBundle = RoomBundleManager.getInstance().getBundle(voucher.getData());
+
+                try {
+                    int roomId = RoomManager.getInstance().createRoom(roomBundle.getConfig().getRoomName().replace("%username%", client.getPlayer().getData().getUsername()), "", roomBundle.getRoomModelData(), 0, 20, 0, client, roomBundle.getConfig().getThicknessWall(), roomBundle.getConfig().getThicknessFloor(), roomBundle.getConfig().getDecorations(), roomBundle.getConfig().isHideWalls());
+
+                    for (RoomBundleItem roomBundleItem : roomBundle.getRoomBundleData()) {
+                        long newItemId = ItemDao.createItem(client.getPlayer().getId(), roomBundleItem.getItemId(), roomBundleItem.getExtraData());
+
+                        if (roomBundleItem.getWallPosition() == null) {
+                            RoomItemDao.placeFloorItem(roomId, roomBundleItem.getX(), roomBundleItem.getY(), roomBundleItem.getZ(), roomBundleItem.getRotation(), roomBundleItem.getExtraData(), newItemId);
+                        } else {
+
+                            RoomItemDao.placeWallItem(roomId, roomBundleItem.getWallPosition(), roomBundleItem.getExtraData(), newItemId);
+                        }
+                    }
+
+                    client.send(new RoomForwardMessageComposer(roomId));
+                    client.send(new EnforceRoomCategoryMessageComposer());
+                    client.send(new BoughtItemMessageComposer(BoughtItemMessageComposer.PurchaseType.BADGE));
+                    client.getPlayer().setLastRoomCreated((int) Comet.getTime());
+
+                } catch (Exception e) {
+                    client.send(new MotdNotificationMessageComposer("Invalid room bundle data, please contact an administrator."));
+                    client.send(new BoughtItemMessageComposer(BoughtItemMessageComposer.PurchaseType.BADGE));
+                }
+                break;
+            }
         }
 
         if(failure) {
