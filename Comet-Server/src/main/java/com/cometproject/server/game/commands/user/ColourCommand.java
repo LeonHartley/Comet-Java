1563cae5b7d629951a0f373b0cf9fcfa5319c81f
ryanleet <ryanleetweb@gmail.com>
2017-01-06 03:53:04 +0100

    Chat colour command improvements

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/user/ColourCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/user/ColourCommand.java
@@ -2,6 +2,7 @@ package com.cometproject.server.game.commands.user;
 
 import com.cometproject.server.config.Locale;
 import com.cometproject.server.game.commands.ChatCommand;
+import com.cometproject.server.game.rooms.types.components.types.ChatMessage;
 import com.cometproject.server.game.rooms.types.components.types.ChatMessageColour;
 import com.cometproject.server.network.sessions.Session;
 
@@ -10,8 +11,42 @@ public class ColourCommand extends ChatCommand {
     public void execute(Session client, String[] params) {
         if (params.length != 1) {
             client.getPlayer().setChatMessageColour(null);
+			
+			sendNotif(Locale.getOrDefault("command.colour.reset", "Your chat has been returned to normal!"), client);
+			return;
+        }
 
-            sendNotif(Locale.getOrDefault("command.colour.reset", "Your chat has been returned to normal!"), client);
+        String colourString = "";
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.red", "red"))) {
+            params[0] = "red";
+            colourString = (Locale.getOrDefault("command.colour.red", "red"));
+        }
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.blue", "blue"))) {
+            params[0] = "blue";
+            colourString = (Locale.getOrDefault("command.colour.blue", "blue"));
+        }
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.green", "green"))) {
+            params[0] = "green";
+            colourString = (Locale.getOrDefault("command.colour.green", "green"));
+        }
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.purple", "purple"))) {
+            params[0] = "purple";
+            colourString = (Locale.getOrDefault("command.colour.purple", "purple"));
+        }
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.cyan", "cyan"))) {
+            params[0] = "cyan";
+            colourString = (Locale.getOrDefault("command.colour.cyan", "cyan"));
+        }
+
+        if (params[0].toLowerCase().equals(Locale.getOrDefault("command.colour.reset.word", "normal"))) {
+            client.getPlayer().setChatMessageColour(null);
+
+            sendNotif(Locale.getOrDefault("command.colour.reset", "Your chat colour has been returned to normal!"), client);
             return;
         }
 
@@ -24,7 +59,7 @@ public class ColourCommand extends ChatCommand {
 
             sendNotif(Locale.getOrDefault(
                     "command.colour.done",
-                    "Your chat messages are now %colour%").replace("%colour%", colour.toString().toLowerCase()), client);
+                    "Your chat messages are now %colour%").replace("%colour%", colourString), client);
         } catch (Exception e) {
             sendNotif(Locale.getOrDefault(
                     "command.colour.invalid",
