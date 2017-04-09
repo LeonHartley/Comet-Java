c2cfbcc38f82174c3f90fbbf393945c0bf048001
Leon Hartley <lhartley97@gmail.com>
2017-04-09 21:24:53 +0100

    Coerce stuff

--- Comet-Server/src/main/java/com/cometproject/server/network/NetworkManager.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/NetworkManager.java
@@ -1,6 +1,8 @@
 package com.cometproject.server.network;
 
 import com.cometproject.api.messaging.console.ConsoleCommandRequest;
+import com.cometproject.api.messaging.exec.ExecCommandRequest;
+import com.cometproject.api.messaging.exec.ExecCommandResponse;
 import com.cometproject.api.messaging.performance.QueryRequest;
 import com.cometproject.api.messaging.status.StatusRequest;
 import com.cometproject.api.messaging.status.StatusResponse;
@@ -33,6 +35,9 @@ import io.netty.util.internal.logging.Log4JLoggerFactory;
 import org.apache.log4j.Logger;
 import org.xbill.DNS.Address;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.UUID;
@@ -77,14 +82,40 @@ public class NetworkManager {
         this.serverPort = Integer.parseInt(ports.split(",")[0]);
 
         try {
-            this.messagingClient = MessagingClient.create("com.cometproject:instance/" + Comet.instanceId + "/" + CometSettings.hotelName.replace(" ", "-").toLowerCase(), new CoerceConfiguration("configuration/Coerce.json"));
+            this.messagingClient = MessagingClient.create("com.cometproject:instance/" + Comet.instanceId + "/" +
+                    "" + CometSettings.hotelName.replace(" ", "-").toLowerCase(),
+                    new CoerceConfiguration("configuration/Coerce.json"));
 
             this.messagingClient.observe(ConsoleCommandRequest.class, (consoleCommandRequest -> {
                 ConsoleCommands.handleCommand(consoleCommandRequest.getCommand());
             }));
 
+            this.messagingClient.observe(ExecCommandRequest.class, (execRequest -> {
+                final String command = execRequest.getCommand();
+
+                try {
+                    Process process = Runtime.getRuntime().exec(command);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            process.getInputStream()));
+
+                    StringBuilder commandOutput = new StringBuilder();
+                    String buffer;
+
+                    while ((buffer = reader.readLine()) != null) {
+                        commandOutput.append(buffer).append("\n");
+                    }
+
+                    this.messagingClient.sendResponse(execRequest.getMessageId(), execRequest.getSender(),
+                            new ExecCommandResponse(commandOutput.toString()));
+                } catch (IOException e) {
+                    this.messagingClient.sendResponse(execRequest.getMessageId(), execRequest.getSender(),
+                            new ExecCommandResponse("Exception: " + e));
+                }
+            }));
+
             this.messagingClient.observe(StatusRequest.class, (statusRequest -> {
-                messagingClient.sendResponse(statusRequest.getMessageId(), statusRequest.getSender(), new StatusResponse(Comet.getStats(), Comet.getBuild()));
+                messagingClient.sendResponse(statusRequest.getMessageId(), statusRequest.getSender(),
+                        new StatusResponse(Comet.getStats(), Comet.getBuild()));
             }));
 
             final InetAddress address = InetAddress.getLocalHost();
