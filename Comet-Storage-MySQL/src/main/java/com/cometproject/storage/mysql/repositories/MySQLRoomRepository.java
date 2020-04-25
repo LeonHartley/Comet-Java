9f7fb93091525eeceb186332bd6b90431832960e
Leon Hartley <leon.hartley@degree53.com>
2020-04-25 22:21:17 +0100

    Fix room data update query + 2.11.4-TEST3

--- Comet-Storage-MySQL/src/main/java/com/cometproject/storage/mysql/repositories/MySQLRoomRepository.java
+++ Comet-Storage-MySQL/src/main/java/com/cometproject/storage/mysql/repositories/MySQLRoomRepository.java
@@ -110,8 +110,7 @@ public class MySQLRoomRepository extends MySQLRepository implements IRoomReposit
                         "group_id = ?, " +
                         "required_badge = ?, " +
                         "thumbnail = ?, " +
-                        "hide_wired = ?, " +
-                        "disabled_commands = ? " +
+                        "hide_wired = ? " +
                         "WHERE id = ?;",
                 data.getName(),
                 data.getDescription(),
@@ -145,7 +144,6 @@ public class MySQLRoomRepository extends MySQLRepository implements IRoomReposit
                 data.getRequiredBadge(),
                 data.getThumbnail(),
                 data.isWiredHidden() ? "1" : "0",
-                "0",
                 data.getId());
 
     }
